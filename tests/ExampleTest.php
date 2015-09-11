2629c18dff2cfb11195ab779c8944853f0dcce71
emeka <emekaosuagwuandela@gmail.com>
2015-09-11 15:21:11 +0000

    Fix home test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,6 +11,6 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('SUYABAY RELOADED');
+             ->see('My List');
     }
 }
