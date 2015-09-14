7ff53f62fe134dba5769b4e0e462ab19f8207953
andela-iadeniyi <ibonly01@gmail.com>
2015-09-14 17:30:36 +0100

    [#103078628]Hotfix: Remove password reset

--- app/User.php
+++ app/User.php
@@ -35,5 +35,5 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
+    //protected $hidden = ['password', 'remember_token'];
 }
