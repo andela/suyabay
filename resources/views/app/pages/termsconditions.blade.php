314b894a33fe4968c7aca7ed0e40a3b4b479641a
andela-aiwewale <iwewale1@gmail.com>
2015-09-16 12:07:37 +0100

    [#103078790] Add terms and condition page

---
+++ resources/views/app/pages/termsconditions.blade.php
@@ -0,0 +1,29 @@
+@extends('app.master')
+
+@section('title', 'About SuyaBay')
+
+@section('content')
+
+<div class="row">
+
+<div class="col s3">
+ @include('app.includes.sections.side_nav')
+</div>
+<div class="col s8">
+  <h3>Terms and Conditions</h3>
+    <p>
+      This site or application ("Website") is owned or managed by SuyaBay. SuyaBay owns and operates this website. All references hereafter to SuyaBay shall include SuyaBay and, where appropriate, its employees, officers, directors, investors, agents, consultants, assignees, affiliates, partners, contractors, attorneys, accountants, advertisers, and any and all other individuals and organizations providing services on behalf of SuyaBay.
+    </p>
+    <p>
+      SuyaBay provides this Website and related services for your personal non-commercial use only and subject to your compliance with this Terms of Use Agreement (the "Agreement"). Please read this Agreement carefully before using this Website. Your use of this Website constitutes your acceptance to be bound by this Agreement without limitation, qualification or change. If at any time you do not accept all the terms and conditions of this Agreement, you must immediately discontinue use of this Website. This Agreement sets forth SuyaBay's policies with respect to its operation of the Website. Other policies govern SuyaBay's non-Internet operations.
+      Please e-mail any questions regarding this Agreement using the "Contact Us" section of the Site. You may have this same information sent to you by e-mail by sending a letter to legal@SuyaBay.com.
+    </p>
+
+
+</div>
+
+<div class="col s1">
+</div>
+
+</div>
+@endsection
