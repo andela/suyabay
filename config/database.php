a03b0c17f2edc5993ca80fb0f74606715d655764
andela-iadeniyi <ibonly01@gmail.com>
2015-10-19 12:11:24 +0100

    Modify tavis.yml and database.php for database testing on TRAVIS

--- config/database.php
+++ config/database.php
@@ -67,10 +67,10 @@ return [
         ],
 
         'testing' => [
-            'driver'    => 'mysql',
+            'driver'    => 'pgsql',
             'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'forge'),
-            'username'  => env('DB_TEST_USERNAME', 'forge'),
+            'database'  => env('DB_TEST_DATABASE', 'suyabay_test'),
+            'username'  => env('DB_TEST_USERNAME', 'postgres'),
             'password'  => env('DB_TEST_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
