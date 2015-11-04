a8ac2c1d404bb8681a7830915273a6238c941ab7
andela-fokosun <okosunuzflorence@gmail.com>
2015-11-04 08:56:06 +0100

    modified root files

--- resources/views/app/includes/contents/main.blade.php
+++ resources/views/app/includes/contents/main.blade.php
@@ -6,7 +6,9 @@
             <div class="collection">
                 <a href="#" class="collection-item">Channels <span class="new badge">4</span></a>
                 <a href="#" class="collection-item">Favourites <span class="new badge">0</span></a>
-                <a href="#" class="collection-item" id="view-all-episodes">See all episodes <span class="badge">10+</span></a>
+                <a href="#" class="collection-item" id="view-all-episodes">See all episodes
+                    <span class="badge">10+</span>
+                </a>
                 <a href="{{ URL::to('about') }}" class="collection-item">About</a>
                 <a href="{{ URL::to('privacypolicy') }}" class="collection-item">Privacy Policy</a>
                 <a href="{{ URL::to('faqs') }}" class="collection-item">FAQs</a>
