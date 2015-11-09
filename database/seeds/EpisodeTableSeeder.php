3d377b4d715746102aa3b6b9f202cdc1ef72384e
andela-fokosun <okosunuzflorence@gmail.com>
2015-11-09 15:52:33 +0100

    [Suyabay]hotfix: Resolve issues with migration

--- database/seeds/EpisodeTableSeeder.php
+++ database/seeds/EpisodeTableSeeder.php
@@ -11,6 +11,6 @@ class EpisodeTableSeeder extends Seeder
      */
     public function run()
     {
-            factory(Suyabay\Episode::class, 1)->create();
+            factory(Suyabay\Episode::class, 20)->create();
     }
 }
