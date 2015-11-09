c361e5e96aa330aa363ed95f267aff98ad197780
andela-iadeniyi <ibonly01@gmail.com>
2015-11-09 23:10:06 +0100

    [Hotfix] Resolve raised issues

--- resources/views/app/pages/signup.blade.php
+++ resources/views/app/pages/signup.blade.php
@@ -84,15 +84,16 @@
     </div>
 
     <div class="col s6 offset-s3">
-        <a href="{!!URL::to('login/facebook')!!}">
+        <a href="{{ URL::to('login/facebook') }}">
             <div class="facebook">
                 <div class="col s3"><i class="fa fa-facebook fa-2x"></i></div>
                 <div class="col s9">Login with Facebook</div>
             </div>
         </a>
     </div>
+
     <div class="col s6 offset-s3">
-        <a href="{!!URL::to('login/twitter')!!}">
+        <a href="{{ URL::to('login/twitter') }}">
             <div class="twitter">
                 <div class="col s3"><i class="fa fa-twitter fa-2x"></i></div>
                 <div class="col s9">Login with Twitter</div>
