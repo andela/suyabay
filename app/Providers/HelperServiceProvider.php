c19838df85d605e08a7282e9758ce6a9a643e9f9
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-11-24 19:53:00 +0100

    [HotFix][#108214826] create helper function

---
+++ app/Providers/HelperServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Suyabay\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelperServiceProvider extends ServiceProvider
+{
+  /**
+   * Bootstrap the application services.
+   *
+   * @return void
+   */
+  public function boot() {
+    //
+  }
+  /**
+   * Register the application services.
+   *
+   * @return void
+   */
+  public function register() {
+    foreach ( glob(app_path() . '/Helpers/*.php') as $filename ) {
+      require_once ($filename);
+    }
+  }
+}
--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -8,25 +8,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 
         <!-- HTTP -->
-        <script src = "{!! asset('/js/jquery.min.js') !!}"></script>
+        <script src = "{!! load_asset('/js/jquery.min.js') !!}"></script>        
+        
 
-        <!-- HTTPS -->
-        <script src = "{!! secure_asset('/js/jquery.min.js') !!}"></script>
-
-        <link href="{!! asset('/library/sweetalert/sweetalert.css') !!}" rel='stylesheet' type="text/css">
-        <script src = "{!! asset('/library/sweetalert/sweetalert.min.js') !!}"></script>
+        <link href="{!! load_asset('/library/sweetalert/sweetalert.css') !!}" rel='stylesheet' type="text/css">
+        <script src = "{!! load_asset('/library/sweetalert/sweetalert.min.js') !!}"></script>
 
         <!-- Fonts and style rules -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,700,600italic,700italic,800,800italic' rel='stylesheet' type='text/css'>
 
         <!-- HTTP -->
-        <link href="{!! asset('/css/materialize.css') !!}" rel='stylesheet' type="text/css">
-        <!-- HTTPS -->
-        <link href="{!! secure_asset('/css/materialize.css') !!}" rel='stylesheet' type="text/css">
+        <link href="{!! load_asset('/css/materialize.css') !!}" rel='stylesheet' type="text/css">
+        
 
-        <link href = "{!! asset('/css/main.css') !!}" rel='stylesheet' type="text/css">
-        <link href = "{!! secure_asset('/css/main.css') !!}" rel='stylesheet' type="text/css">
+        <link href = "{!! load_asset('/css/main.css') !!}" rel='stylesheet' type="text/css">
+        
 
         <!-- Font Awesome-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -34,28 +31,12 @@
 
         <!-- Materialize Compiled and minified JavaScript -->
         <!-- HTTP -->
-        <script src = "{!! asset('/js/materialize.min.js') !!}"></script>
-        <!-- HTTPS -->
-        <script src = "{!! secure_asset('/js/materialize.min.js') !!}"></script>
+        <script src = "{!! load_asset('/js/materialize.min.js') !!}"></script>    
 
 
+        <script src = "{!! load_asset('/js/dashboard.js') !!}"></script>
 
-        <!--
-        Custom scripts for SUYABAY dashboard
-        Javascript
-        CSS
-        -->
-
-        <!-- 
-        javascripts 
-        -->
-        <script src = "{!! asset('/js/dashboard.js') !!}"></script>
-        
-
-        <!-- 
-        css 
-        -->
-        <link href = "{!! asset('/css/dashboard.css') !!}" rel='stylesheet' type="text/css">
+        <link href = "{!! load_asset('/css/dashboard.css') !!}" rel='stylesheet' type="text/css">
 
 
     </head>
