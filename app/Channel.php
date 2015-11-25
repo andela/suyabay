42bc68bbafb048c833e61ed4d8c979a1f8bd66d7
andela-iadeniyi <ibonly01@gmail.com>
2015-11-25 21:11:54 +0100

    [feature/AdminChannel][#108215326] Implement channel create function using AJAX

--- app/Channel.php
+++ app/Channel.php
@@ -6,6 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'channels';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['channel_name', 'channel_description', 'subscription_count'];
+
+    /**
+     * Channel/Episode relationship
+     */
     public function episodes()
     {
         return $this->hasMany('Suyabay\Episode');
--- resources/views/dashboard/includes/contents/create_channel.blade.php
+++ resources/views/dashboard/includes/contents/create_channel.blade.php
@@ -1,8 +1,8 @@
-<!-- 
+<!--
  # Author     Emeka Osuagwu <emekaosuagwuandela0@gmail.com>
  # Copyright  2015 Emeka Osuagwu
- # License    MIT License <http://opensource.org/licenses/MIT>   
- 
+ # License    MIT License <http://opensource.org/licenses/MIT>
+
  #CodeFuntion: This piece of code is resposible for the structure
  of the create Episode page on the application
  -->
@@ -12,29 +12,30 @@
     <div class="row">
         <h4>Create Channel</h4><br>
         <div class="row">
-            <form class="col s12">
-                
+            <form class="col s12" id="create_channel" action="/dashboard/channel/create" method="post">
+                <input type="hidden" value="{{ csrf_token() }}" name="_token" id="token" />
+
                 <div class="row">
                     <div class="input-field col s12">
-                        <input placeholder="Channel Title" id="first_name" type="text" class="validate">
-                        <label for="first_name">Channel Title</label>
+                        <input placeholder="Channel Title" name="name" id="name" type="text" class="validate" require="true">
+                        <label for="name">Channel Title</label>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="input-field col s12">
-                    <input disabled. id="disabled" type="text" placeholder="Channel Description" class="validate">
-                    <label for="disabled">Channel Description</label>
+                    <textarea id="description" name="description" placeholder="Channel Description" require="true"></textarea>
+                    <label for="description">Channel Description</label>
                     </div>
                 </div><br>
 
                 <center>
-                    <a href="/dashboard/channels" class="waves-effect waves-light btn"><i class="material-icons right"></i>Create</a>
+                    <button type="submit" class="waves-effect waves-light btn"><i class="material-icons right"></i>Create</button>
                 </center>
 
             </form>
         </div>
-        
+
     </div>
 
 
