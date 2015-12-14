2bed823bec2936f801b86ff72bc734442ec72f51
andela-iadeniyi <ibonly01@gmail.com>
2015-12-14 16:51:41 +0100

    (Hotfix) Modify roles migration

--- database/migrations/2015_11_29_213317_create_roles_table.php
+++ database/migrations/2015_11_29_213317_create_roles_table.php
@@ -13,7 +13,7 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-             $table->integer('id')->change();
+             $table->integer('id');
             $table->string('name')->nullable()->unique();
         });
     }
