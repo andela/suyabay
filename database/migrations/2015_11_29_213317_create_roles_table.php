97577756b3958caa87b88a85d6ca601ecc3905f1
andela-iadeniyi <ibonly01@gmail.com>
2015-12-14 16:49:25 +0100

    (Hotfix) Remove role seeder and update roles migration

--- database/migrations/2015_11_29_213317_create_roles_table.php
+++ database/migrations/2015_11_29_213317_create_roles_table.php
@@ -13,7 +13,7 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->change();
+             $table->integer('id')->change();
             $table->string('name')->nullable()->unique();
         });
     }
