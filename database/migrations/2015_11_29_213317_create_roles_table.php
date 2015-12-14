27819b46ef5a1267cc4a752cc3c28280e84dbfbd
andela-iadeniyi <ibonly01@gmail.com>
2015-12-14 13:44:58 +0100

    (Hotfix) Create Seeder class for Role table

--- database/migrations/2015_11_29_213317_create_roles_table.php
+++ database/migrations/2015_11_29_213317_create_roles_table.php
@@ -13,7 +13,7 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('id')->unsigned()->change();
             $table->string('name')->nullable()->unique();
         });
     }
--- database/seeds/EpisodeTableSeeder.php
+++ database/seeds/EpisodeTableSeeder.php
@@ -14,5 +14,6 @@ class EpisodeTableSeeder extends Seeder
         factory(Suyabay\Channel::class, 20)->create();
         factory(Suyabay\Episode::class, 20)->create();
         factory(Suyabay\Comment::class, 20)->create();
+        factory(Suyabay\Role::class, 1)->create();
     }
 }
