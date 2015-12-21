983c94694e0298d369b7d937442406b90273f044
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-21 19:50:51 +0100

    [Fixes #108215092]Modified Notifications

--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -1,11 +1,10 @@
 <div class="row">
     <div class="col s12">
-        <h4>NEW EPISODE NOTIFICATION</h4>
-    </div>
-    <div class="col s12">
     <p>
         <h3>A new Episode has been created awaiting your approval.</h3>
         <ul>
+            <li><b>Created by:</b> {{ Auth::user()->username }} </li>
+            <li><b>Date:</b> {{ date('Y-m-d H:i:s') }} </li>
             <li><b>Title:</b> {{ $title }} </li>
             <li><b>Description:</b> {{ $description}} </li>
             <li><b>Channel:</b> {{ $channel }} </li>
