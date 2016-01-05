c564566fe3a1ef97e62adbb20e0b9617eaf966d7
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-01-05 14:51:28 +0100

    [Hotfix:DashBoardActivityPage] Fixed Nitpick Ci issues

--- app/Http/Controllers/EpisodeManager.php
+++ app/Http/Controllers/EpisodeManager.php
@@ -34,9 +34,8 @@ class EpisodeManager extends Controller
         $episodes           = Episode::get();
         $active_episode     = $episodes->where('status', 1);
         $pendding_episode   = $episodes->where('status', 0);
-        
-        $channels           = Channel::get();
 
+        $channels           = Channel::get();
 
         $data = 
         [
@@ -220,7 +219,6 @@ class EpisodeManager extends Controller
         $active_episode     = $episodes->where('status', 1);
         $pendding_episode   = $episodes->where('status', 0);
 
-
         $data = 
         [
             "user" => 
@@ -236,7 +234,6 @@ class EpisodeManager extends Controller
                         "active"    => $active_episode,
                         "pending"   => $pendding_episode
                     ]
-
         ];
         return $data;
         //return view('dashboard/pages/index', compact('data'));
