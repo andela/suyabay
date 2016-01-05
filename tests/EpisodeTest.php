d60c6a28054de60ec110d63be3770cc0ab9327ae
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-05 13:13:14 +0100

    [Chore][#108215092]create unit tests for create episode route

---
+++ tests/EpisodeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Suyabay\Episode;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class EpisodeTest extends TestCase
+{
+    /**
+     * Test that an episode is created.
+     *
+     * @return void
+     */
+    public function testCreateNewEpisode()
+    {
+        Episode::create(['episode_name' => 'test', 'episode_description' => 'test', 'channel_id' => 1, 'view_count' => 0, 'image' => 'dummy url', 'audio_mp3' => 'dummy url', 'status' => 0]);
+
+        $this->seeInDatabase('episodes', ['episode_name' => 'test', 'episode_description' => 'test', 'channel_id' => 1, 'view_count' => 0, 'image' => 'dummy url', 'audio_mp3' => 'dummy url', 'status' => 0]);
+    }
+
+    /**
+     * Test that only authenticated user can use this route
+     * status code
+     * @return status code
+     */
+    public function testApplication()
+    {
+        $response = $this->call('GET', 'dashboard/episode/create', ['username' => 'Taylor', 'password' => 'pass']);
+
+        $this->assertEquals(302, $response->status());
+    }
+
+    /**
+     * Test that only authenticated user can use this route
+     *
+     * @return void
+     */
+    public function testNewEpisodeCreateWorkflow()
+    {
+        $this->visit('/dashboard/episode/create', ['username' => 'Taylor', 'password' => 'pass'])->seePageIs('/login');
+    }
+}
