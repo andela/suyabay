f55ae45e33c10c5ce1f3ebb83a8b6d21b4b4371b
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-01-06 09:37:34 +0100

    [Hotfix:DashBoardActivityPage] Refactor Channel Model

--- app/Channel.php
+++ app/Channel.php
@@ -18,13 +18,7 @@ class Channel extends Model
      *
      * @var array
      */
-    protected $fillable = 
-            [
-                'channel_name', 
-                'channel_description', 
-                'subscription_count',
-                'user_id'
-            ];
+    protected $fillable = ['channel_name', 'channel_description', 'subscription_count', 'user_id'];
 
     /**
      * Channel/Episode relationship
