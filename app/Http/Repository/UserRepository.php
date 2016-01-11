1bc2aa083f8e21a3340d36d6d451ec9ef26536c6
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-01-11 11:48:01 +0100

    [Hotfix: DashBoardActivityPage] Refactor EpisodeManager

--- app/Http/Repository/UserRepository.php
+++ app/Http/Repository/UserRepository.php
@@ -10,25 +10,25 @@ class UserRepository
     /**
     * Return all user from the database
     */
-	public function getAllUsers()
-	{
-		return User::all();
-	}
+    public function getAllUsers()
+    {
+        return User::all();
+    }
 
     /**
     * Return all online users
     */
-	public function getOnlineUsers()
-	{
-		return $this->getAllUser()->where('active', 1);
-	}
+    public function getOnlineUsers()
+    {
+        return $this->getAllUser()->where('active', 1);
+    }
 
     /**
     * Return all offline users
     */
-	public function getOfflineUsers()
-	{
-		return $this->getAllUser()->where('active', 0);
-	}
+    public function getOfflineUsers()
+    {
+        return $this->getAllUser()->where('active', 0);
+    }
 
 }
