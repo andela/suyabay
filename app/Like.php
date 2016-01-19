9f9267218caac40072025566d9b4777172bc9b53
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-01-19 11:28:17 +0100

    [Feature:FavoriteEpisode][#111946023] Add Like model

---
+++ app/Like.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Suyabay;
+
+use Suyabay\Episode;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'likes';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'episode_id'];
+
+    public function episode()
+    {
+        return $this->belongsTo('Suyabay\Episode');
+    }
+
+}
