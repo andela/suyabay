5e5f846cbf028e625ea4db8908680da909075033
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-01-25 10:21:01 +0100

    [Hotfix][#111946023] replace asset() with load_asset()

--- resources/views/app/master.blade.php
+++ resources/views/app/master.blade.php
@@ -45,7 +45,7 @@
         <script src = "{!! asset('/js/main.js') !!}"></script>
         <script src = "{!! secure_asset('/js/main.js') !!}"></script>
         <script src="{!! asset('/js/audio.min.js') !!}"></script>
-        <script src="{!! asset('/js/like.js') !!}"></script>
+        <script src="{!! load_asset('/js/like.js') !!}"></script>
 
 
     </head>
