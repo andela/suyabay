3d1a8484eace9a32b0c96248cd3ce17079b16aac
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-27 19:46:53 +0100

    [fixes][#112126869]Fix Nitpick Issues

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -228,16 +228,13 @@ class ChannelController extends Controller
             $swap = Episode::where('channel_id', $request->channel_id)->update(['channel_id' => $request->new_channel_id]);
             $this->channel->deleteChannel($request->channel_id);
             Episode::where('channel_id', $request->channel_id)->update(['flag' => 1]);
+            $this->response = ['message' => 'Episodes swapped and Channel deleted Successfully!', 'status_code' => 200];
+        } catch (QueryException $e) {
             $this->response = [
-                    'message'     => 'Episodes swapped and Channel deleted Successfully!',
-                    'status_code' => 200
-                ];
-            } catch (QueryException $e) {
-                $this->response = [
-                    'message'       => 'Channel already exist',
-                    'status_code'   => 400
-                ];
-            }
+                'message'       => 'Channel already exist',
+                'status_code'   => 400
+            ];
+        }
 
         return $this->response;
     }
