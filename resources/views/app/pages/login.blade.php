0c0f5dae64642902186e6b3b000fb10cb0ddb267
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-01-27 08:28:03 +0100

    [Hotfix] fix nitpick issue

--- resources/views/app/pages/login.blade.php
+++ resources/views/app/pages/login.blade.php
@@ -63,7 +63,6 @@
     </div>
 
         <!-- social login -->
-
         <div class="col s6 offset-s3">
             <a href="{{ URL::to('login/facebook') }}">
                 <div class="facebook">
