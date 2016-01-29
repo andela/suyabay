8df71ac4ae22ea278a045ff28ecfd078e65507a2
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-29 14:38:17 +0000

    [Fixes][#112667707]Fix error on review app

--- resources/views/app/includes/sections/top_nav.blade.php
+++ resources/views/app/includes/sections/top_nav.blade.php
@@ -7,26 +7,17 @@
             <a href="/" id="logo-container">
                 <img src="{!! asset('/css/logo.png') !!}" class="logo" />
             </a>
-
             <ul class="right hide-on-med-and-down">
                 <li>
                     <form action="/search" method="POST">
                     {{ csrf_field() }}
-
                         <div class="input-field">
-
                             <input name = "data" id="search" type="search" class="search" required>
-
                             <label for="search">
-
                                 <i class="material-icons teal-text text-lighten-2">search</i>
-
                             </label>
-
                             <i class="material-icons">close</i>
-
                         </div>
-
                     </form>
                 </li>
 
