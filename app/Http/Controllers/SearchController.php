9f4c8d28f005711f937606dd83f6e1af33ad18f0
andela-cvundi <christophervundi@gmail.com>
2016-02-04 09:44:20 +0300

    [Feature: #112579483] Add search controller to make searches possible

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,20 +3,40 @@
 namespace Suyabay\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Suyabay\Http\Requests;
-use Suyabay\Http\Controllers\Controller;
+use Suyabay\Episode;
+use Suyabay\Channel;
 
 class SearchController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Perform a search query and return results
+     * if any otherwise notify user No results.
      */
-    public function index()
+    public function getResults(Request $request)
     {
-        return redirect('/');
+        $query = $request->input('query');
 
+        if (!$query) {
+            return redirect()
+            ->route('home')
+            ->with('info', 'Could not find what you searched for');
+        }
+
+        if (env('APP_ENV') === 'testing') {
+            $results = Episode::where('episode_name', 'LIKE', "%{$query}%")
+                            ->orWhere('episode_description', 'LIKE', "%{$query}%")
+                            ->get();
+        } else {
+            $results = Episode::where('episode_name', 'ILIKE', "%{$query}%")
+                            ->orWhere('episode_description', 'ILIKE', "%{$query}%")
+                            ->get();
+        }
+
+        $channels = Channel::all();
+
+        return view('app.pages.search')->with([
+            'results' => $results,
+            'channels' => $channels,
+        ]);
     }
-
 }
