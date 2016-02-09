b9921ed307077a2562fc13bb407ef148080786c7
John Kariuki <john.kariuki@andela.com>
2016-02-09 16:13:24 +0300

    [Fix][#112876091] Fix reload page on new comment. remove edit button.. edit with enter remove delete confirm and do not reload

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,48 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$(".comment-submit").on("click", function(){
-		
-		var comment       = $("#comment-field").val();
-		var avatar        = $(this).data("avatar");
-		var comment_count = parseInt($(this).data("comment-count")) + 1;
-		var token         = $(this).data("token");
+    $(".comment-submit").on("click", function() {
 
-		var url        = "/comment";
-		var user_id    = $("#user_id").val();
-		var episode_id = $("#episode_id").val();
+        var comment = $("#new-comment-field").val();
+        var avatar = $(this).data("avatar");
+        var comment_count = parseInt($(this).data("comment-count")) + 1;
+        var token = $(this).data("token");
 
-		var data =
-			{
-				parameter  :
-					{
-						_token     : token,
-						user_id    : user_id,
-						episode_id : episode_id,
-						comment    : comment
-					}
-			}
-		if (comment.length > 0) {
-			//Process AJAX request
-			$.ajax({
-				url    : url,
-				type   : "POST",
-				data   : data.parameter,
+        var url = "/comment";
+        var user_id = $("#user_id").val();
+        var episode_id = $("#episode_id").val();
 
-				success: function(response) {
-					switch (response.status_code)
-					{
-						case 200:
+        var data = {
+            parameter: {
+                _token: token,
+                episode_id: episode_id,
+                comment: comment
+            }
+        }
+        if (comment.length > 0) {
+            //Process AJAX request
+            $.ajax({
+                url: url,
+                type: "POST",
+                data: data.parameter,
 
-							location.reload();
-						break;
+                success: function(response) {
+                    switch (response.status_code) {
+                        case 200:
 
-						default: return false;
-					}
-				}
-			});
-		}
+                            //Increment comment upon successful submission
+                            $("#comment-count").html(" " + comment_count);
 
-		return false;
-	});
+                            var newComment = '<div id="show_comment" class="collection-item avatar show_comment">';
+                            newComment += '<div class="row">';
+                            newComment += '<div class="col s2"><img src="' + avatar + '" alt="" class="circle"></div>';
+                            newComment += '<div class="col s10">';
+                            newComment += '<div class="textarea-wrapper" data-comment-id="' + response.commentId + '">';
+                            newComment += '<span>' + comment + '</span>';
+
+
+                            newComment += '<div class="update-actions pull-right">';
+                            newComment += ' <a href="#" id="comment_action_caret" class="fa fa-bars no-style-link"></a> ';
+                            newComment += ' <div id="comment_actions" style="display:none">';
+                            newComment += '<a href="#" class="fa fa-pencil comment-action-edit no-style-link" data-commentId="' + response.commentId + '"></a>';
+                            newComment += '<a href="#" class="fa fa-trash comment-action-delete no-style-link" data-commentId="' + response.commentId + '"></a>';
+                            newComment += '</div>';
+                            newComment += '</div>';
+                            newComment += '</div></div></div></div>';
+
+                            $(".load_comment").last().append(newComment);
+                            $("#new-comment-field").val("");
+
+                            break;
+
+                        default:
+                            return false;
+                    }
+                }
+            });
+        }
+
+        return false;
+    });
 
 });
