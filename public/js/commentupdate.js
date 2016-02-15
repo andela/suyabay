b8c95edb5c251bf47a9fa91a4a29d553ba1b7bed
John Kariuki <john.kariuki@andela.com>
2016-02-15 23:48:32 +0300

    [Fix][#112876091] Fix Javascript styling issues

--- public/js/commentupdate.js
+++ public/js/commentupdate.js
@@ -4,49 +4,57 @@ $(document).ready(function() {
 
         event.preventDefault();
 
-        $('.textarea-wrapper #comment_actions').not($(this).next()).hide('slow');
+        $('.textarea-wrapper #comment_actions')
+            .not($(this)
+                .next())
+            .hide('slow');
 
         $(this).next().toggle('slow');
     });
 
-    $('.load_comment').on('click', '#comment_actions a.comment-action-delete', function(event) {
+    $('.load_comment').on('click',
+        '#comment_actions a.comment-action-delete',
+        function(event) {
 
-        event.preventDefault();
+            event.preventDefault();
 
-        var this_ = $(this);
-        var token = $('.load_comment').attr('data-token');
-        var comment = this_.attr('data-commentId');
+            var this_ = $(this);
+            var token = $('.load_comment').attr('data-token');
+            var comment = this_.attr('data-commentId');
 
-        swal({
-            title: 'Are you sure?',
-            text: 'You will not be able to recover a deleted comment',
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#DD6B55',
-            confirmButtonText: 'Yes, delete it!',
-            closeOnConfirm: true
-        }, function() {
+            swal({
+                title: 'Are you sure?',
+                text: 'You will not be able to recover a deleted comment',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#DD6B55',
+                confirmButtonText: 'Yes, delete it!',
+                closeOnConfirm: true
+            }, function() {
 
-            //delete comment
-            var deleteComment = $.ajax({
-                method: 'DELETE',
-                url: '/comment/' + comment,
-                data: {
-                    _token: token
-                }
-            });
+                //delete comment
+                var deleteComment = $.ajax({
+                    method: 'DELETE',
+                    url: '/comment/' + comment,
+                    data: {
+                        _token: token
+                    }
+                });
 
-            deleteComment.done(function() {
-                this_.parent().parent().parent().parent().parent().parent().remove();
+                deleteComment.done(function() {
+                    this_.parent().parent().parent().parent().parent().parent().remove();
 
-            });
+                });
 
-            deleteComment.fail(function() {
+                deleteComment.fail(function() {
 
-                swal('Error Deleting', 'Something happened while deleting your comment. please try again', 'error');
+                    swal('Error Deleting',
+                        'Something happened while deleting your comment. please try again',
+                        'error'
+                    );
+                });
             });
         });
-    });
 
     $('.load_comment').on('click', '#comment_actions a.comment-action-edit', function(event) {
 
