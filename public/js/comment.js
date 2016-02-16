6ee426cdc1c4975d3ee509cf9103d1ab98cf706a
John Kariuki <john.kariuki@andela.com>
2016-02-16 10:07:43 +0300

    [Fix][#112876091] Fix Javascript styling issues

--- public/js/comment.js
+++ public/js/comment.js
@@ -34,25 +34,26 @@ $(document).ready(function() {
 
                             var newComment = '<div id="show_comment" class="collection-item avatar show_comment">';
                             newComment += '<div class="row">';
-                            newComment += '<div class="col s2"><img src="' + avatar + '" alt="" class="circle"></div>';
+                            newComment += '<div class="col s2">';
+                            newComment += '<img src="' + avatar + '" alt="" class="circle">';
+                            newComment += '</div>';
                             newComment += '<div class="col s10">';
-                            newComment += '<div class="textarea-wrapper" data-comment-id="' + response.commentId + '">';
+                            newComment += '<div class="textarea-wrapper" ';
+                            newComment += 'data-comment-id="' + response.commentId + '">';
                             newComment += '<span>' + comment + '</span>';
-
-
                             newComment += '<div class="update-actions pull-right">';
-                            newComment += ' <a href="#" id="comment_action_caret" class="fa fa-bars no-style-link"></a> ';
-                            newComment += ' <div id="comment_actions" style="display:none">';
-                            newComment += '<a href="#" class="fa fa-pencil comment-action-edit no-style-link" data-commentId="';
-                            newComment += response.commentId + '"></a>';
-                            newComment += '<a href="#" class="fa fa-trash comment-action-delete no-style-link" data-commentId="';
-                            newComment += response.commentId + '"></a>';
+                            newComment += '<a href="#" id="comment_action_caret" class="fa fa-bars no-style-link"></a>';
+                            newComment += '<div id="comment_actions" style="display:none">';
+                            newComment += '<a href="#" class="fa fa-pencil comment-action-edit no-style-link" ';
+                            newComment += 'data-commentId="' + response.commentId + '"></a>';
+                            newComment += '<a href="#" class="fa fa-trash comment-action-delete no-style-link" ';
+                            newComment += 'data-commentId="' + response.commentId + '"></a>';
                             newComment += '</div>';
                             newComment += '</div>';
                             newComment += '</div></div></div></div>';
 
-                            $(".load_comment").last().append(newComment);
-                            $("#new-comment-field").val("");
+                            $('.load_comment').last().append(newComment);
+                            $('#new-comment-field').val('');
 
                             break;
 
--- public/js/commentupdate.js
+++ public/js/commentupdate.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
 
-    $('.load_comment').on('click', 'a#comment_action_caret', function(event) {
+    $('.load_comment').on(
+        'click',
+        'a#comment_action_caret',
+        function(event) {
 
-        event.preventDefault();
+            event.preventDefault();
 
-        $('.textarea-wrapper #comment_actions')
-            .not($(this)
-                .next())
-            .hide('slow');
+            $('.textarea-wrapper #comment_actions')
+                .not($(this)
+                    .next())
+                .hide('slow');
 
-        $(this).next().toggle('slow');
-    });
+            $(this).next().toggle('slow');
+        });
 
-    $('.load_comment').on('click',
+    $('.load_comment').on(
+        'click',
         '#comment_actions a.comment-action-delete',
         function(event) {
 
@@ -42,97 +46,111 @@ $(document).ready(function() {
                 });
 
                 deleteComment.done(function() {
-                    this_.parent().parent().parent().parent().parent().parent().remove();
+                    this_
+                        .parent()
+                        .parent()
+                        .parent()
+                        .parent()
+                        .parent()
+                        .parent()
+                        .remove();
 
                 });
 
                 deleteComment.fail(function() {
 
                     swal('Error Deleting',
-                        'Something happened while deleting your comment. please try again',
+                        'Error deleting your comment. please try again',
                         'error'
                     );
                 });
             });
         });
 
-    $('.load_comment').on('click', '#comment_actions a.comment-action-edit', function(event) {
+    $('.load_comment').on(
+        'click',
+        '#comment_actions a.comment-action-edit',
+        function(event) {
 
-        event.preventDefault();
+            event.preventDefault();
 
-        var parent = $(this)
-            .parent()
-            .parent()
-            .parent();
+            var parent = $(this)
+                .parent()
+                .parent()
+                .parent();
 
-        var commentWrapper = parent.find('span:first');
-        var comment = commentWrapper.text().trim();
+            var commentWrapper = parent.find('span:first');
+            var comment = commentWrapper.text().trim();
 
-        parent.find('.update-actions').hide('slow', function() {
+            parent.find('.update-actions').hide('slow', function() {
 
-            var updateComment = '<div class="file-field input-field">';
-            updateComment += '<div class="file-path-wrapper input-field col s10 m10">';
-            updateComment += '<input name="comment" id="comment-field" class="validate" type="text"';
-            updateComment += 'style="margin-left:20px;" required="true" value="' + comment + '"/>';
-            updateComment += '</div>';
-            updateComment += '</div>';
+                var updateComment = '<div class="file-field input-field">';
+                updateComment += '<div class="file-path-wrapper input-field col s10 m10">';
+                updateComment += '<input name="comment" id="comment-field" ';
+                updateComment += 'type="text" style="margin-left:20px;" required ';
+                updateComment += 'class="validate" value="' + comment + '"/>';
+                updateComment += '</div>';
+                updateComment += '</div>';
 
 
-            commentWrapper.replaceWith(updateComment);
+                commentWrapper.replaceWith(updateComment);
+            });
         });
-    });
 
-    $('.load_comment').on('keypress', '#comment-field', function(event) {
+    $('.load_comment').on(
+        'keypress',
+        '#comment-field',
+        function(event) {
 
-        if (event.which == 13) {
+            if (event.which == 13) {
 
-            var this_ = $(this);
+                var this_ = $(this);
 
-            var comment = this_.val().trim();
-            var commentId = this_
-                .parent()
-                .parent()
-                .parent()
-                .attr('data-comment-id');
+                var comment = this_.val().trim();
+                var commentId = this_
+                    .parent()
+                    .parent()
+                    .parent()
+                    .attr('data-comment-id');
 
-            var token = $('.load_comment').attr('data-token');
+                var token = $('.load_comment').attr('data-token');
 
-            if (comment.length === 0 || commentId.length === 0 || token.length === 0) {
-                swal('Error Updating',
-                    'Something is missing in your comment. Please try again',
-                    'error'
-                );
-            } else {
-
-                var updateComment = $.ajax({
-                    method: 'PUT',
-                    url: '/comment/' + commentId + '/edit',
-                    data: {
-                        _token: token,
-                        comment: comment
-                    }
-                });
-
-                updateComment.done(function() {
-
-                    swal({
-                        title: 'Updated!',
-                        text: 'Comment successfully updated',
-                        type: 'success'
-                    }, function() {
-                        location.reload();
-                    });
-
-                });
-
-                updateComment.fail(function() {
-
+                if (comment.length === 0 || commentId.length === 0 || token.length === 0) {
                     swal('Error Updating',
-                        'Something happened while updating your comment. Please try again',
+                        'Something is missing in your comment. Please try again',
                         'error'
                     );
-                });
+                } else {
+
+                    var updateComment = $.ajax({
+                        method: 'PUT',
+                        url: '/comment/' + commentId + '/edit',
+                        data: {
+                            _token: token,
+                            comment: comment
+                        }
+                    });
+
+                    updateComment.done(function() {
+
+                        swal({
+                            title: 'Updated!',
+                            text: 'Comment successfully updated',
+                            type: 'success'
+                        }, function() {
+                            location.reload();
+                        });
+
+                    });
+
+                    updateComment.fail(function() {
+
+                        swal('Error Updating',
+                            'Error updating your comment. Please try again',
+                            'error'
+                        );
+                    });
+                }
             }
-        }
-    });
+        });
 });
