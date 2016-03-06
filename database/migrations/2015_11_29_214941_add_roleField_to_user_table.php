24607f7fb5ab0ff15a30873f9a4cf7ce603ba63a
Raimi Ademola <demola.nig@gmail.com>
2016-03-06 16:10:32 +0100

    [Features #115109857] Fix unnecessary error  displayed in homepage when there are no episodes

--- database/migrations/2015_11_29_214941_add_roleField_to_user_table.php
+++ database/migrations/2015_11_29_214941_add_roleField_to_user_table.php
@@ -13,7 +13,7 @@ class AddRoleFieldToUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role_id')->default(1);
+            $table->integer('role_id')->unsigned()->default(1);
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
