9070c4629138ede0e0dce7b555df3c904d50bed5
andela-tolotin <temitope.olotin@andela.com>
2016-03-07 09:44:52 +0100

    feature #113846287] Remove episode comments back to episode controller

--- resources/views/app/includes/contents/single_episode.blade.php
+++ resources/views/app/includes/contents/single_episode.blade.php
@@ -64,7 +64,7 @@
                                 <ul class="collection">
 
                                 <li class="load_comment" data-token="{{ csrf_token() }}">
-                                    @foreach ( $episodes->first()->comment()->orderBy('created_at', 'asc')->take(10)->get() as $comment )
+                                    @foreach ( $firstTenEpisodes as $comment )
                                         <div id="show_comment" class="collection-item avatar show_comment">
                                             <div class="row">
                                                 <div class="col s2">
@@ -97,7 +97,7 @@
                                 </li>
                                     @if ( Auth::check())
                                         <input type="hidden" id="episode_id" value="{{ $lu = $episodes->first()->comment()->orderBy('created_at', 'asc')->take(10)->get()}} {{ $lu[0]['episode_id'] }}" />
-                                            @if(count($episodes->first()->comment()->orderBy('created_at', 'asc')->take(10)->get()) > 0)
+                                            @if(count($firstTenEpisodes) > 0)
                                                 <li>
                                                     <div class="view_more_comments" data-avatar="{{ Auth::user()->getAvatar() }}">
                                                         <a href="#" title="View more comments">
