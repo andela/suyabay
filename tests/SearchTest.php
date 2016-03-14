7fa633bc5524e87eb8e5e912a4ee2c3b77ea1319
andela-cvundi <christophervundi@gmail.com>
2016-03-14 10:27:22 +0300

    (tests)[#11540232] Assert that user gets no episodes message when searching for a non existent episode

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -23,12 +23,12 @@ class SearchTest extends TestCase
     }
 
     /**
-     * Test that when a user searches for an episode,it
-     * returns all episodes that match the pattern.
+     * Test that when a user searches for an episode by episode
+     * name ,it returns all episodes that match the pattern.
      *
-     * @return [type] [description]
+     * @return void
      */
-    public function testResults()
+    public function testSearchByEpisodeName()
     {
         factory('Suyabay\User')->create();
         factory('Suyabay\Channel')->create();
@@ -40,5 +40,47 @@ class SearchTest extends TestCase
         );
         $this->assertViewHasAll(['results', 'channels']);
         $this->see($episode['episode_name']);
+    }
+
+    /**
+     * Test that when a user searches for an episode with a episode
+     *  description,it returns all episodes that match the description.
+     *
+     * @return void
+     */
+    public function testSearchByEpisodeDescription()
+    {
+        factory('Suyabay\User')->create();
+        factory('Suyabay\Channel')->create();
+        $episode = factory('Suyabay\Episode')->create();
+
+        $this->call(
+            'GET',
+            '/search?query=' .$episode['episode_description'] . '&search='
+        );
+        $this->assertViewHasAll(['results', 'channels']);
+        $this->see($episode['episode_description']);
+    }
+
+    /**
+     * Assert that The text No results found is displayed when one enters search
+     * terms which don't match any episode name or description
+     *
+     * @return void
+     */
+    public function testSearchForNonExistentEpisode()
+    {
+        factory('Suyabay\User')->create();
+        factory('Suyabay\Channel')->create();
+        $episode = factory('Suyabay\Episode')->create();
+
+        $this->call(
+            'GET',
+            '/search?query=SwankyNonExistentEpisode&search='
+        );
+        $this->assertViewHasAll(['results', 'channels']);
+        $this->dontSee($episode['episode_name']);
+        $this->dontSee($episode['episode_description']);
+        $this->see('No results found, sorry');
     }
 }
