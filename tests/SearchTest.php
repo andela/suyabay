deac1935b3c0be4338d10cef9cf4c2e0d7cc94c3
andela-cvundi <christophervundi@gmail.com>
2016-03-14 10:14:12 +0300

    (tests)[#11540232] Assert that user can search for an episode by name

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -9,7 +9,7 @@ class SearchTest extends TestCase
      * Anyone can search episodes by name
      * Enter the episode name then hit enter.
      */
-    public function testHome()
+    public function testSearchLink()
     {
         $this->createUser(1);
         $this->createChannel();
@@ -20,5 +20,25 @@ class SearchTest extends TestCase
              ->type('terms', 'query')
              ->press('search')
              ->seePageIs('/search?query=terms&search=');
+    }
+
+    /**
+     * Test that when a user searches for an episode,it
+     * returns all episodes that match the pattern.
+     *
+     * @return [type] [description]
+     */
+    public function testResults()
+    {
+        factory('Suyabay\User')->create();
+        factory('Suyabay\Channel')->create();
+        $episode = factory('Suyabay\Episode')->create();
+
+        $this->call(
+            'GET',
+            '/search?query=' .$episode['episode_name'] . '&search='
+        );
+        $this->assertViewHasAll(['results', 'channels']);
+        $this->see($episode['episode_name']);
     }
 }
