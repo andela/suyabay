ea1a10f93e2a685a3162abcbcf3d544f5789920d
andela-tolotin <temitope.olotin@andela.com>
2016-03-22 14:46:21 +0100

    [Feature #115755943] Fix issues on psr-2 coding style

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -47,7 +47,7 @@ Route::group(['prefix' => 'api/v1/'], function () {
 |--------------------------------------------------------------------------
 
 */
-Route::group(['prefix' => 'api/v1/'], function() {
+Route::group(['prefix' => 'api/v1/'], function () {
 
     Route::get('channels', function () {
 
