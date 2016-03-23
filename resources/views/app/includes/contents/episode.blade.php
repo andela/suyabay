88e278a8c045a90fe350ac14cbe9010cf6f0fa5d
andela-cvundi <christophervundi@gmail.com>
2016-03-23 11:52:26 +0300

    [Fix] Update channel view page

--- resources/views/app/includes/contents/episode.blade.php
+++ resources/views/app/includes/contents/episode.blade.php
@@ -8,63 +8,40 @@
 
         <h4 class="center-align padcast-page-header" style="margin-bottom:50px;">Podcast for Suya lovers</h1>
 
+        @if ($episodes->count() > 0)
+            @foreach($episodes as $podcast)
+            <a style="color:#2C3E50" href="/episodes/{{$podcast->id}}">
+                <div class="row podcast">
+                    <div class="col s3">
+                        <img class="responsive-img podcast-img" src="{{ asset($podcast->image) }}">
+                    </div>
 
-        @foreach($episodes as $podcast)
-        <a style="color:#2C3E50" href="/episodes/{{$podcast->id}}">
-            <div class="row podcast">
-            <div class="col s3">
-                <a style="color:#2C3E50" href="/episodes/{{$podcast->id}}">
-                    <img class="responsive-img podcast-img" src="{!! asset($podcast->image) !!}">
-                </a>
-            </div>
+                    <div class="col s9 details">
+                        <span class="podcast-episode-date">{{ $podcast->created_at->diffForHumans() }}</span>
+                        <span class="tag podcast-episode-date">{{$podcast->channel->channel_name}}</span>
+                        <h5 class="podcast-episode-title">{{ $podcast->episode_name }}</h5>
 
-            <div class="col s9 details">
-                <span class="podcast-episode-date">{{$podcast->created_at->diffForHumans()}}</span>
-                <span class="tag podcast-episode-date">{{$podcast->channel->channel_name}}</span>
-                <a style="color:#2C3E50" href="/episodes/{{$podcast->id}}">
-                    <h5 class="podcast-episode-title">{{$podcast->episode_name}}</h5>
-                </a>
-                <div>
-                    <audio width="10px;" src="{{$podcast->audio_mp3}}" preload="auto" />
+                        <p>
+                            {{$podcast->episode_description}}
+                        </p>
+
+                    </div>
                 </div>
-                <p>
-                     {{$podcast->episode_description}}
-                </p>
-
-                <div class="podcast-actions">
-
-                    <input type="hidden" id="token" name="_token" value="{{ csrf_token() }}">
-
-                    @if( Auth::check() )
-                        <input type="hidden" id="user_id" value="{{ Auth::user()->id }}" >
-                    @endif
-
-                    <input type="hidden" id="episode_id" value="{{ $podcast->id }}">
-
-                    <span style="padding-right:15px;">
-                         <i class="fa fa-heart social-btn like-btn {{ $podcast->like_status }}" like-status="{{ $podcast->like_status }}"> {{ $podcast->likes }}</i>
-                    </span>
-
-                    <span style="padding-right:15px;">
-                        <i class="fa fa-comment social-btn like"> {{ $podcast->comment->count() }}</i>
-                    </span>
-
-                    <span style="padding-right:15px;">
-                        <a href="#" class="twtr-share" data-desc="{{ $podcast->episode_description }}" data-name="{{ $podcast->episode_name }}" data-img="{!! asset($podcast->image) !!}" data-url="{!! url('/episodes', $podcast->id)  !!}">
-                            <i class="fa fa-twitter social-btn "></i>
-                        </a>
-                    </span>
-                    <span style="padding-right:15px;">
-                        <a href="#" class="fb-share" data-desc="{{ $podcast->episode_description }}" data-name="{{ $podcast->episode_name }}" data-img="{!! asset($podcast->image) !!}" data-url="{!! url('/episodes', $podcast->id) !!}">
-                            <i class="fa fa-facebook social-btn "></i>
-                        </a>
-                    </span>
+            </a>
+            @endforeach
+        @else
+            <div class="col s12">
+                <div class="card-panel grey lighten-5 z-depth-1">
+                  <div class="row valign-wrapper">
+                    <div class="col s12">
+                      <h4 class="black-text center">
+                        <i class="fa fa-info-circle teal-text"></i> Ooops, there are no episodes in this channel
+                      </h4>
+                    </div>
+                  </div>
                 </div>
-            </div>
-        </div>
-        </a>
-        @endforeach
-
+              </div>
+        @endif
 
         <p class="center-align">
             <a href="/" class=" waves-effect waves-light btn">Back home</a>
