5b3e4dd5036104777432eed9158c8b459bb0677d
John Kariuki <john.kariuki@andela.com>
2016-03-29 11:40:08 +0300

    [Chore 116206695] Maintain standard single quotes in like.js

--- public/js/like.js
+++ public/js/like.js
@@ -1,41 +1,42 @@
 
 $(document).ready(function() {
 
-    $(".like-btn").click(function () {
+    $('.like-btn').click(function () {
 
         var this_ = $(this);
-        var status        = $(this).attr("like-status");
-        var likeCount    = $(this).html();
-        var favoriteCount = $("#favorite").html();
 
-        if (status === "like")
+        var status        = $(this).attr('like-status');
+        var likeCount    = $(this).html();
+        var favoriteCount = $('#favorite').html();
+
+        if (status === 'like')
         {
-            $(this).removeClass("like");
-            $(this).addClass("dislike");
-            $(this).attr("like-status", "dislike");
+            $(this).removeClass('like');
+            $(this).addClass('dislike');
+            $(this).attr('like-status', 'dislike');
 
             likeEpisode();
 
             likeCount = Number(likeCount) + 1;
             favoriteCount = Number(favoriteCount) + 1;
 
-            $(this).text( " " + likeCount);
-            $("#favorite").html(favoriteCount);
+            $(this).text( ' ' + likeCount);
+            $('#favorite').html(favoriteCount);
         }
 
-        if (status === "dislike")
+        if (status === 'dislike')
         {
-            $(this).removeClass("dislike");
-            $(this).addClass("like");
-            $(this).attr("like-status", "like");
+            $(this).removeClass('dislike');
+            $(this).addClass('like');
+            $(this).attr('like-status', 'like');
 
             dislikeEpisode(this_);
 
             likeCount = Number(likeCount) - 1;
             favoriteCount = Number(favoriteCount) - 1;
 
-            $(this).text(" " + likeCount);
-            $("#favorite").html(favoriteCount);
+            $(this).text(' ' + likeCount);
+            $('#favorite').html(favoriteCount);
 
             //check if the string favorites is present in the current url
             if (/favorites/.test(window.location.pathname)) {
@@ -43,9 +44,9 @@ $(document).ready(function() {
             }
         }
 
-        if (status === "must_login")
+        if (status === 'must_login')
         {
-            window.location = "/login";
+            window.location = '/login';
         }
 
     });
@@ -55,19 +56,14 @@ $(document).ready(function() {
     */
     function likeEpisode()
     {
-        var url             = "/episode/like";
-        var token           = document.getElementById("token").value;
-        var method          = "POST";
-
         var data =
         {
-            url             : url,
-            method          : method,
-            parameter       :
-            {
-              _token        : token,
-              user_id       : document.getElementById("user_id").value,
-              episode_id    : document.getElementById("episode_id").value
+            url             : '/episode/like',
+            method          : 'POST',
+            parameter       : {
+              _token        : document.getElementById('token').value,
+              user_id       : document.getElementById('user_id').value,
+              episode_id    : document.getElementById('episode_id').value
             }
         };
 
@@ -79,19 +75,14 @@ $(document).ready(function() {
     */
     function dislikeEpisode(this_)
     {
-        var url             = "/episode/unlike";
-        var token           = document.getElementById("token").value;
-        var method          = "post";
-
         var data =
         {
-            url             : url,
-            method          : method,
-            parameter       :
-            {
-              _token        : token,
-              user_id       : document.getElementById("user_id").value,
-              episode_id    : this_.attr("data-episode-id")
+            url             : '/episode/unlike',
+            method          : 'POST',
+            parameter       : {
+              _token        : document.getElementById('token').value,
+              user_id       : document.getElementById('user_id').value,
+              episode_id    : this_.attr('data-episode-id')
             }
         };
 
@@ -112,7 +103,7 @@ $(document).ready(function() {
             },
             error: function()
             {
-                alert("Are you sure you doing this the right way?");
+                alert('Are you sure you doing this the right way?');
             },
         });
     }
