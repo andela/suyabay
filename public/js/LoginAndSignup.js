d03049f5e228793c64c289bef31dc5b85571c99e
John Kariuki <john.kariuki@andela.com>
2016-03-29 12:55:05 +0300

    [Chore 116206695] Fix single and double quote standard.

--- public/js/LoginAndSignup.js
+++ public/js/LoginAndSignup.js
@@ -14,23 +14,24 @@
 | @response is the ajax response coming from the api end ponit
 | @funtionName is the name of the fuction called
 */
-function ajaxLogic ( data, response, functionName )
-{
-    if ( response.status_code === 401)
-    {
-        switch (functionName)
-        {
-          case "login"    : loginErrorAlert(); break;
-          case "register" : registerErrorAlert(); break;
+function ajaxLogic(data, response, functionName) {
+    if (response.status_code === 401) {
+        switch (functionName) {
+            case 'login':
+                loginErrorAlert();
+                break;
+            case 'register':
+                registerErrorAlert();
+                break;
         }
-    }
-
-    else if ( response.status_code === 200)
-    {
-        switch (functionName)
-        {
-          case "login"    :  window.location="/"; break;
-          case "register" :  registerSuccessAlert(data); break;
+    } else if (response.status_code === 200) {
+        switch (functionName) {
+            case 'login':
+                window.location = '/';
+                break;
+            case 'register':
+                registerSuccessAlert(data);
+                break;
         }
     }
 
@@ -44,27 +45,24 @@ function ajaxLogic ( data, response, functionName )
 | @data is the array of user infomation \\console.log(data) to see properties
 | @funtionName is the name of the fuction called
 |*/
-function ajaxCall ( data, functionName )
-{
+function ajaxCall(data, functionName) {
     $('.loader').show();
-    $.post( data.url, data.parameter )
-    .done(function(response)
-    {
-      ajaxLogic( data, response, functionName );
-    })
-    .fail(function(response) {
-      console.log('this action is bad')
-    })
+    $.post(data.url, data.parameter)
+        .done(function(response) {
+            ajaxLogic(data, response, functionName);
+        })
+        .fail(function(response) {
+            console.log('this action is bad')
+        })
 }
 
 /*
 | loginErrorAlert
 | gives error report to user
 */
-function loginErrorAlert ()
-{
-  $('.loader').hide();
-  swal("Oops! Login Failed", "Username or Password not found!", "error");
+function loginErrorAlert() {
+    $('.loader').hide();
+    swal('Oops! Login Failed', 'Username or Password not found!', 'error');
 }
 
 /*
@@ -72,23 +70,23 @@ function loginErrorAlert ()
 | gives Success report to user
 | receives 1 parameter
 */
-function registerSuccessAlert (data)
-{
-  swal({
-    title: data.parameter.username + " Your SuyaBay account has been successfully created",
-    text: "Send Email Confirmation",
-    type: "success",
-    showCancelButton: true,
-    closeOnConfirm: false,
-    showLoaderOnConfirm: true,
-  },
-  function(){
-    setTimeout(function(){
-      swal("Email Confirmation sent to " + data.parameter.email);
-    }, 2000);
-    clearField();
-    window.location="/";
-  });
+function registerSuccessAlert(data) {
+    swal({
+            title: data.parameter.username +
+                ' Your SuyaBay account has been successfully created',
+            text: 'Send Email Confirmation',
+            type: 'success',
+            showCancelButton: true,
+            closeOnConfirm: false,
+            showLoaderOnConfirm: true,
+        },
+        function() {
+            setTimeout(function() {
+                swal('Email Confirmation sent to ' + data.parameter.email);
+            }, 2000);
+            clearField();
+            window.location = '/';
+        });
 }
 
 /*
@@ -96,109 +94,91 @@ function registerSuccessAlert (data)
 | gives error report to user
 | receives 1 parameter
 */
-function registerErrorAlert ()
-{
-  $('.loader').hide();
-  swal("Oops! Registration Failed", "Username or Email already exists click the button to try again!", "error");
+function registerErrorAlert() {
+    $('.loader').hide();
+    swal(
+        'Oops! Registration Failed',
+        'Username or Email already exists click the button to try again!',
+        'error'
+    );
 }
 
 /*
 | register
 | create user information and url in to an array of object i.e @data
-| and make and ajax class to function ajaxCall() by sending @data and @functionName
+| and make and ajax class to function ajaxCall()
+| by sending @data and @functionName
 */
-function register ()
-{
-  var url       = "/signup";
-  var email     = $('#email').val();
-  var token     = $('#token').val();
-  var username  = $('#username').val();
-  var password  = $('#password').val();
-  var facebook  = $('#facebook').val();
-  var twitter   = $('#twitter').val();
-
-  var data =
-    {
-        url         : url,
-        parameter   :
-        {
-          _token      : token,
-          email       : email,
-          username    : username,
-          password    : password,
-          facebook    : facebook,
-          twitter     : twitter
+function register() {
+    var data = {
+        url: '/signup',
+        parameter: {
+            _token: $('#token').val(),
+            email: $('#email').val();,
+            username: $('#username').val(),
+            password: $('#password').val(),
+            facebook: $('#facebook').val(),
+            twitter: $('#twitter').val()
         }
     }
     preventFormDefault('.form')
     checkItem(data);
-    var functionName =  arguments.callee.name;
-    ajaxCall( data, functionName );
+    var functionName = arguments.callee.name;
+    ajaxCall(data, functionName);
 }
 
 
 /*
 | login
 | create user information and url in to an array of object i.e @data
-| and make and ajax class to function ajaxCall() by sending @data and @functionName
+| and make and ajax class to function ajaxCall()
+| by sending @data and @functionName
 */
-function login ()
-{
-    var url       = '/login';
-    var email     = $('#email').val();
-    var token     = $('#token').val();
-    var username  = $('#username').val();
-    var password  = $('#password').val();
-
-    var functionName =  arguments.callee.name;
-    var data =
-    {
-        url         : url,
-        parameter   :
-        {
-          _token      : token,
-          username    : username,
-          password    : password
+function login() {
+    var functionName = arguments.callee.name;
+    var data = {
+        url: '/login',
+        parameter: {
+            _token: ('#token').val(),
+            username: $('#username').val(),
+            password: $('#password').val()
         }
     }
     preventFormDefault('.form');
-    ajaxCall( data, functionName );
+    ajaxCall(data, functionName);
 }
 
-function checkItem (data)
-{
+function checkItem(data) {
 
-      if  ( data.parameter.email == '' || data.parameter.username == '' || data.parameter.password == '' )
-      {
-        swal("Oops!", "Some required field not set!", "error");
+    if (
+        data.parameter.email == '' ||
+        data.parameter.username == '' || data.parameter.password == '') {
+        swal('Oops!', 'Some required field not set!', 'error');
         end();
-      }
+    }
 
-      if ( ! /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.parameter.email) )
-      {
-          swal("Oops!", "Invalid email", "error");
-          end();
-      }
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+            data.parameter.email)) {
+        swal('Oops!', 'Invalid email', 'error');
+        end();
+    }
 }
 
 /*
 | Prevent element Default action
 */
-function preventFormDefault (element)
-{
-  $(element).submit(function(e)
-  {
-    e.preventDefault();
-  });
+function preventFormDefault(element) {
+    $(element).submit(function(e) {
+        e.preventDefault();
+    });
 }
 
 /*
 | Clears field
 */
-function clearField ()
-{
-  $('#email').val('');
-  $('#token').val('');
-  $('#username').val('');
-  $('#password').val('');
+function clearField() {
+    $('#email').val('');
+    $('#token').val('');
+    $('#username').val('');
+    $('#password').val('');
 }
