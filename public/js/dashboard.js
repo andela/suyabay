06c1006b60e58966d9bdda21f9c09629e901d569
John Kariuki <john.kariuki@andela.com>
2016-03-29 09:17:09 +0300

    [Chore 116206695] Fix control structure opening bracket position and fux variable camelcasing

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,232 +1,206 @@
+$(document).ready(function() {
 
-$( document ).ready(function() {
+    var item;
 
-	var item;
+    $('.episode_action').on('change', function() {
 
-	$('.episode_action').on('change', function(){
-
-    	/*
+        /*
 		# Get <select/> element that was clicked on
     	*/
-    	action_type = $(this).val(),
+        actionType = $(this).val(),
 
-    	/*
+            /*
 		# Get <select/> element data-action
     	*/
-    	action = $(this).find('option:selected').data('action');
+        action = $(this).find('option:selected').data('action');
 
-		/*
+        /*
 		# Add class to parent element of the <select/> element
     	*/
-    	$(this).parent().closest('tr').prop("class", "selected");
+        $(this).parent().closest('tr').prop("class", "selected");
 
 
-    	if ( action_type === "view" )
-    	{
-    		window.location = action;
-    	}
+        if (actionType === "view") {
+            window.location = action;
+        }
 
-    	if ( action_type === "delete" )
-    	{
-    		deleteEpisode(action)
-    	};
+        if (actionType === "delete") {
+            deleteEpisode(action)
+        };
 
-    	if ( action_type === "activate" )
-    	{
+        if (actionType === "activate") {
 
-    		item = $(this).parent().closest('tr');
-    		activateEpisode(action)
-    	};
-	});
+            item = $(this).parent().closest('tr');
+            activateEpisode(action)
+        };
+    });
 
 
-	/*
-	# Delete Episode Function
-	*/
-	function deleteEpisode (episode_id)
-	{
-		var
-		url 		= "/dashboard/episode/delete",
-		token 		= document.getElementById('token').value,
-		method 		= "DELETE",
-		episode_id 	= episode_id,
-		functionName =  arguments.callee.name;
+    /*
+    # Delete Episode Function
+    */
+    function deleteEpisode(episodeId) {
+        var
+            url = "/dashboard/episode/delete",
+            token = document.getElementById('token').value,
+            method = "DELETE",
+            episode_id = episodeId,
+            functionName = arguments.callee.name;
 
-	  	var data =
-	    {
-	        url         : url,
-	        method 		: method,
-	        parameter   :
-	        {
-	          _token 	  : token,
-	          episode_id  : episode_id
-	        }
-	    }
+        var data = {
+            url: url,
+            method: method,
+            parameter: {
+                _token: token,
+                episode_id: episodeId
+            }
+        }
 
-		swal(
-		{
-			title: "Delete Episode",
-			text: "You will not be able to recover this episode after this action!",
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonColor: "#DD6B55",
-			confirmButtonText: "Yes, delete it!",
-			cancelButtonText: "No, cancel!",
-			closeOnCancelnConfirm: false,
-			closeOnCancel: false
-		},
-		function(isConfirm)
-		{
-		  if (isConfirm)
-		  {
-		    ajaxCall( data, functionName)
-		  }
-		  else
-		  {
-			swal("Cancelled", "Your episode is safe :)", "error");
-		  }
-		});
-	}
+        swal({
+                title: "Delete Episode",
+                text: "You will not be able to recover this episode!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete it!",
+                cancelButtonText: "No, cancel!",
+                closeOnCancelnConfirm: false,
+                closeOnCancel: false
+            },
+            function(isConfirm) {
+                if (isConfirm) {
+                    ajaxCall(data, functionName)
+                } else {
+                    swal("Cancelled", "Your episode is safe :)", "error");
+                }
+            });
+    }
 
-	/*
-	# Delete Episode Function
-	*/
-	function activateEpisode (episode_id)
-	{
-		var
-		url 		= "/dashboard/episode/activate",
-		token 		= document.getElementById('token').value,
-		method 		= "PATCH",
-		functionName =  arguments.callee.name;
+    /*
+    # Delete Episode Function
+    */
+    function activateEpisode(episodeId) {
+        var
+            url = "/dashboard/episode/activate",
+            token = document.getElementById('token').value,
+            method = "PATCH",
+            functionName = arguments.callee.name;
 
-	  	var data =
-	    {
-	        url         : url,
-	        method 		: method,
-	        parameter   :
-	        {
-	          _token 	  : token,
-	          episode_id  : episode_id
-	        }
-	    }
+        var data = {
+            url: url,
+            method: method,
+            parameter: {
+                _token: token,
+                episode_id: episodeId
+            }
+        }
 
-		swal(
-		{
-			title: "Activate Episode",
-			text: "This Episode will be visible on your channel",
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonColor: "success",
-			confirmButtonText: "Activate",
-			cancelButtonText: "Cancel!",
-			closeOnConfirm: false,
-			closeOnCancel: false
-		},
-		function(isConfirm)
-		{
-		  if (isConfirm)
-		  {
-		    ajaxCall( data, functionName)
-		  }
-		  else
-		  {
-			swal("Cancelled", "Your episode is safe :)", "error");
-		  }
-		});
-	}
+        swal({
+                title: "Activate Episode",
+                text: "This Episode will be visible on your channel",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "success",
+                confirmButtonText: "Activate",
+                cancelButtonText: "Cancel!",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            },
+            function(isConfirm) {
+                if (isConfirm) {
+                    ajaxCall(data, functionName)
+                } else {
+                    swal("Cancelled", "Your episode is safe :)", "error");
+                }
+            });
+    }
 
 
 
-	/*
-	# Ajax
-	*/
-	function ajaxCall ( data, functionName)
-	{
-		$.ajax({
-			url: data.url,
-			type: data.method,
-			data: data.parameter,
-			success: function (response)
-			{
-				ajaxLogic ( response, functionName )
-			},
-			error: function ()
-			{
-				alert('Are you sure you doing this the right way?');
-			},
-		});
-	}
+    /*
+    # Ajax
+    */
+    function ajaxCall(data, functionName) {
+        $.ajax({
+            url: data.url,
+            type: data.method,
+            data: data.parameter,
+            success: function(response) {
+                ajaxLogic(response, functionName)
+            },
+            error: function() {
+                alert('Are you sure you doing this the right way?');
+            },
+        });
+    }
 
 
-	function ajaxLogic ( response, functionName )
-	{
-		/*
-		# Check if Response.status = 401
-		*/
-		if ( response.status === 401 )
-		{
-			switch (functionName)
-	        {
-	          case "deleteEpisode" : deleteEpisodeErrorAlert(); break;
-	          case "activateEpisode" : activateEpisodeErrorAlert(); break;
-	        }
-		}
+    function ajaxLogic(response, functionName) {
+        /*
+        # Check if Response.status = 401
+        */
+        if (response.status === 401) {
+            switch (functionName) {
+                case "deleteEpisode":
+                    deleteEpisodeErrorAlert();
+                    break;
+                case "activateEpisode":
+                    activateEpisodeErrorAlert();
+                    break;
+            }
+        }
 
-		/*
-		# Check if Response.status = 200
-		*/
-		if ( response.status === 200 )
-		{
-			switch (functionName)
-	        {
-	          case "deleteEpisode" : deleteEpisodeSuccessAlert(); break;
-	          case "activateEpisode" : activateEpisodeSuccessAlert(); break;
-	        }
-		}
-	}
+        /*
+        # Check if Response.status = 200
+        */
+        if (response.status === 200) {
+            switch (functionName) {
+                case "deleteEpisode":
+                    deleteEpisodeSuccessAlert();
+                    break;
+                case "activateEpisode":
+                    activateEpisodeSuccessAlert();
+                    break;
+            }
+        }
+    }
 
 
 
 
 
-	/*
-	####################################################################
-	# deleteEpisodeErrorAlert Message
-	*/
-	function deleteEpisodeErrorAlert ()
-	{
-		swal("Hmmmm", "This episode does not exist ", "error");
-	}
+    /*
+    # deleteEpisodeErrorAlert Message
+    */
+    function deleteEpisodeErrorAlert() {
+        swal("Hmmmm", "This episode does not exist ", "error");
+    }
 
-	/*
-	# deleteEpisodeSuccessAlert Message
-	*/
-	function deleteEpisodeSuccessAlert ()
-	{
-		swal("Deleted!", "Your episode has been deleted.", "success");
-		var deleted =  $('.selected');
-		deleted.hide();
-	}
+    /*
+    # deleteEpisodeSuccessAlert Message
+    */
+    function deleteEpisodeSuccessAlert() {
+        swal("Deleted!", "Your episode has been deleted.", "success");
+        var deleted = $('.selected');
+        deleted.hide();
+    }
 
-	/*
-	####################################################################
-	# activateEpisodeErrorAlert() Message
-	*/
-	function activateEpisodeErrorAlert ()
-	{
-		swal("Hmmmm", "This episode does not exist ", "error");
-	}
+    /*
+    # activateEpisodeErrorAlert() Message
+    */
+    function activateEpisodeErrorAlert() {
+        swal("Hmmmm", "This episode does not exist ", "error");
+    }
 
-	function activateEpisodeSuccessAlert()
-	{
-		swal("Activated", "Your episode has been Activated.", "success");
+    function activateEpisodeSuccessAlert() {
+        swal("Activated", "Your episode has been Activated.", "success");
 
-		var deleted 	=  $(".selected");
-		var new_item 	=  $("#active_section");
+        var deleted = $(".selected");
+        var new_item = $("#active_section");
 
-		new_item.append(item[0]);
+        new_item.append(item[0]);
 
-		deleted.show();
-	}
+        deleted.show();
+    }
 
 });
