1e06536216e5cc8abb3e44f4e02c42377e7f67dc
Raimi Ademola <ademola.raimi@andela.com>
2016-04-04 17:35:51 +0100

    [feature #116351243] ensure that we have only one navbar and modify api.main.css

--- resources/views/api/includes/contents/app.blade.php
+++ resources/views/api/includes/contents/app.blade.php
@@ -9,7 +9,7 @@
                 <h5 class="app-title">My Apps</h5>
                 <div class="myapp">
                     <p class="app-text">You haven't created any suyabay apps yet.</p>
-                    <a class="waves-effect waves btn" href="/developer/myapp/register">Create a new app</a>   
+                    <a class="waves-effect waves btn" href="/developer/myapp/new">Create a new app</a>   
                 </div>    
             </div>    
         </div>
@@ -17,7 +17,7 @@
         <div class="col s4">
             <div id="main-two">
                 <div class="session-one">
-                    <a class="waves-effect waves-one btn" href="/developer/myapp/register">Create a new app</a>
+                    <a class="waves-effect waves-one btn" href="/developer/myapp/new">Create a new app</a>
                 </div> 
                 <div class="session-two">
 
@@ -34,16 +34,7 @@
     <!-- top nav -->
         @include("api.includes.sections.top_nav")
 
-    <div class="row">
-        <div class="error-message">
-            <h4 class="error-text">OOps, please kindly sign up if you don't already have an account or login otherwise, in order to view this page</h4>
-            <div class="col s6">
-                <a class="waves-effect waves-two btn" href="/login">Login here </a>
-            </div>
-            <div class="col s6">
-                <a class="waves-effect waves-two btn" href="/signup">Sign up here</a>
-            </div>
-        </div>
-    </div>
+    <!--auth check error page -->    
+        @include("api.includes.contents.autherrorpage")
 
 @endif
--- resources/views/api/includes/contents/main.blade.php
+++ resources/views/api/includes/contents/main.blade.php
@@ -1,6 +1,20 @@
 <!-- top nav -->
 <div class="row">
-    @include("api.includes.sections.top_nav")
+    <div class="navbar-fixed">
+    <nav role="navigation">
+        <div class="nav-wrapper">
+
+            <!-- Desktop view top nav -->
+
+            <a href="/" id="logo-container">
+                <img src="{!! load_asset('/css/logo.png') !!}" class="logo" />
+            </a>
+            <a href="#" data-activates="nav-mobile" class="button-collapse">
+                <i class="material-icons">menu</i>
+            </a>
+        </div>
+    </nav>
+</div>
 </div>
 
 <div id="hero" class="index_bg_container">
