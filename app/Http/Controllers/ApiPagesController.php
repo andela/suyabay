ed220f22b4a1b825fce465528b98ef1bbf0cedaa
Raimi Ademola <ademola.raimi@andela.com>
2016-04-04 18:35:36 +0100

    [feature #116785861] create a form to register a new app

--- app/Http/Controllers/ApiPagesController.php
+++ app/Http/Controllers/ApiPagesController.php
@@ -4,13 +4,33 @@ namespace Suyabay\Http\Controllers;
 
 class ApiPagesController extends Controller
 {
+    /**
+     * Displays the index page of the API.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return view('api.pages.index');
     }
 
+    /**
+     * Display list of apps the user have created and a link to create new appp.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function myApp()
     {
         return view('api.pages.myapp');
+    }
+    
+    /**
+     * Displays a form a form where user can register their new app.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createNewApp()
+    {
+        return view('api.pages.mynewapp');
     }
 }
