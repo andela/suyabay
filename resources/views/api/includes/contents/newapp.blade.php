be501e5cd7e714cc4224aac13b12d4e87da402e0
Raimi Ademola <ademola.raimi@andela.com>
2016-04-05 23:17:01 +0100

    [feature #116351243] modify the form page

--- resources/views/api/includes/contents/newapp.blade.php
+++ resources/views/api/includes/contents/newapp.blade.php
@@ -7,24 +7,25 @@
     	<div class="col s8">
         	<div id="main-new-one">
 	    		<div class='form-feature'>
-			    	<form>
+			    	<form method="POST">
+			    	<input type="hidden" id="token" name="_token" value="{{ csrf_token() }}">
 			    		<div class='form-content'>
 				    		<h5>Application name</h5>
 
 					    		<div class='form-group'>
-					    			<input name='application_name' class='form-control'> </textarea>
+					    			<input name='name' class='form-control' value="{{ old('application_name') }}">
 					    		</div>	
 
 				    		<h5 class='new-app-text'>Home page url</h5>
 
 					    		<div class='form-group'>
-					    			<input name='homepage_url' class='form-control'> </textarea>
+					    			<input name='homepage_url' class='form-control' value="{{ old('homepage_url') }}">
 					    		</div>				   			    
 
 				    		<h5 class='new-app-text'>Application Description</h5>
 				   
 					    		<div class='form-group'>
-					    			<textarea name='application_description' class='form-control'> </textarea>
+					    			<textarea name='description' class='form-control'> </textarea>
 					    		</div>	
 
 					    	<div class='form-group'>
@@ -32,7 +33,6 @@
 					    	</div>	
 				    	</div>
 		    		</form>
-
 	    		</div>
 	    	</div>
 	    </div>  
