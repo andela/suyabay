225fa20a2e622bd9d8f464551aa06aa522af9ffa
Raimi Ademola <ademola.raimi@andela.com>
2016-04-06 19:33:33 +0100

    [feature #116351243] return the branch to its normal state

--- app/Http/Controllers/ApiPagesController.php
+++ app/Http/Controllers/ApiPagesController.php
@@ -31,32 +31,6 @@ class ApiPagesController extends Controller
     {
         return view('api.pages.myapp');
     }
-    
-    /**
-     * Generate a token for user with passed Id.
-     *
-     * @param int $userId
-     *
-     * @return string
-     */
-    public function generateToken()
-    {
-        $appSecret = getenv('APP_SECRET');
-        $jwtAlgorithm = getenv('JWT_ALGORITHM');
-        $timeIssued = time();
-        $tokenId = base64_encode(getenv('TOKENID'));
-        $token = [
-        	'iss'  => 'http://suyabay-staging.herokuapp.com/',
-            'iat'  => $timeIssued,   // Issued at: time when the token was generated
-            'jti'  => $tokenId,          // Json Token Id: an unique identifier for the token
-            'nbf'  => $timeIssued, //Not before time
-            'exp'  => $timeIssued + 60 * 60 * 24 * 30, // expires in 30 days
-            'data' => [                  // Data related to the signer user
-            //'user_id'   => $user_id, // userid from the users table
-            ],
-        ];
-        return JWT::encode($token, $appSecret, $jwtAlgorithm);
-    }
         
     /**
      * Displays a form where user can register their new app.
@@ -67,23 +41,4 @@ class ApiPagesController extends Controller
     {
         return view('api.pages.mynewapp');
     }
-
-    public function showAppInfo()
-    {
-    	$app_infos = AppInfo::all();
-
-    	return view('api.pages.appinfo', compact('app_infos'));
-    }
-
-    public function storeAppInfo(Request $request)
-    {
-    	AppInfo::create([
-            'name'         => $request->name,
-            'homepage_url' => $request->homepage_url,
-            'description'  => $request->description,
-            'user_id'      => $request->user_id,
-        ]);
-
-        return $this->generateToken();
-    }    
 }
