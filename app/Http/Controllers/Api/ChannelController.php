8d8408fe314283c12309e795f0c02f132fd5737b
andela-tolotin <temitope.olotin@andela.com>
2016-04-06 11:51:15 +0100

    [Feature #116864429] Implement postChannel, editChannel, and getASingleChannel methods for channel routes

--- app/Http/Controllers/Api/ChannelController.php
+++ app/Http/Controllers/Api/ChannelController.php
@@ -2,6 +2,7 @@
 
 namespace Suyabay\Http\Controllers\Api;
 
+use DB;
 use Validator;
 use Suyabay\Channel;
 use Suyabay\Http\Requests;
@@ -32,24 +33,16 @@ class ChannelController extends Controller
      *
      * @return json $response
      */
-    public function getAllChannels(Request $request)
+    public function getAllChannels(Request $request, ChannelTransformer $channelTransformer)
     {
         $perPage = $request->query('results') ? : 10;
 
         $channels = Channel::orderBy('id', 'asc')
         ->skip($this->getRecordsToSkip($perPage, $request))
         ->take($perPage)
-        ->get([
-            'id',
-            'channel_name',
-            'channel_description',
-            'subscription_count',
-            'created_at',
-            'updated_at',
-            'user_id',
-        ]);
+        ->get();
 
-         $resource = new Collection($channels, new ChannelTransformer());
+         $resource = new Collection($channels, $channelTransformer);
          $data = $this->fractal->createData($resource)->toArray();
 
          if (count($data['data']) > 0) {
@@ -68,23 +61,15 @@ class ChannelController extends Controller
      *
      * @return json $response
      */
-    public function getAChannel($channel_name)
+    public function getAChannel($channel_name, ChannelTransformer $channelTransformer)
     {
         $channel = Channel::where('channel_name', '=', $channel_name)
         ->orWhere('channel_name', '=', strtolower($channel_name))
         ->orderBy('id', 'asc')
-        ->first([
-            'id',
-            'channel_name',
-            'channel_description',
-            'subscription_count',
-            'created_at',
-            'updated_at',
-            'user_id',
-        ]);
+        ->first();
 
         if (! is_null($channel)) {
-            $resource = new Item($channel, new ChannelTransformer());
+            $resource = new Item($channel, $channelTransformer);
             $data = $this->fractal->createData($resource)->toArray();
 
             return Response::json($data, 200);
@@ -104,22 +89,27 @@ class ChannelController extends Controller
      */
     public function postAChannel(Request $request)
     {
+        $userId = 1;// The id of the user gotten from the token
+
+        if ($this->validateUserRequestForEmptyFields($request)) {
+            return Response::json(['message' => 'All fields are required'], 400);
+
+        }
+
         if ($this->validateUserRequest($request)) {
-            return Response::json(['message' => 'Expected fields not supplied or Channel already exists or may the supplied fields does not contain values'], 400);
+            return Response::json(['message' => 'Channel already exists'], 400);
 
-        } else {
-            $userId = 1;// The id of the user gotten from the token
-            $channel = Channel::create([
-                'channel_name' => $request->input('channel_name'),
-                'channel_description' => $request->input('channel_description'),
-                'created_at' => date('Y-m-d h:i:s'),
-                'user_id' => $userId,
-            ]);
+        } 
 
-            if (count($channel) > 0) {
-                return Response::json(['message' => 'Channel created successfully'], 201);
+        $channel = Channel::create([
+            'channel_name' => $request->input('channel_name'),
+            'channel_description' => $request->input('channel_description'),
+            'created_at' => date('Y-m-d h:i:s'),
+            'user_id' => $userId,
+        ]);
 
-            }
+        if (count($channel) > 0) {
+            return Response::json(['message' => 'Channel created successfully'], 201);
         }
     }
 
@@ -133,26 +123,131 @@ class ChannelController extends Controller
      */
     public function editAChannel(Request $request, $channel_name)
     {
+        if ($this->validateUserRequestForEmptyFields($request)) {
+            return Response::json(['message' => 'All fields are required'], 400);
+
+        }
+
         $channel = Channel::where('channel_name', '=', $channel_name)
         ->orWhere('channel_name', '=', strtolower($channel_name))
-        ->first()
-        ->toArray();
+        ->first();
 
-        if (count($channel) > 0) {
-            
+        if ($this->processEditChannel($request, $channel)) {
+            return Response::json(['message' => 'Channel updated successfully'], 200);
+
         }
 
         return Response::json([
-            'message' => 'Channel cannot be updated because the channel name was incorrect'
+            'message' => 'Channel cannot be updated because the channel name is incorrect'
         ], 404);
+
+    }
+
+    /**
+     * This method updates a channel using PATCH verb
+     *
+     * @param $channel_name
+     *
+     * @return $response
+     */
+    public function editASingleChannelResource(Request $request, $channel_name)
+    {
+        if ($this->validateUserRequestForPatchRequest($request)) {
+            return Response::json(['message' => 'All fields are required'], 400);
+
+        }
+
+        $channel = Channel::where('channel_name', '=', $channel_name)
+        ->orWhere('channel_name', '=', strtolower($channel_name))
+        ->first();
+
+        if ($this->processEditChannelForPatchRequest($request, $channel)) {
+            return Response::json(['message' => 'Channel updated successfully'], 200);
+
+        }
+
+        return Response::json([
+            'message' => 'Channel cannot be updated because the channel name is incorrect'
+        ], 404);
+
+    }
+
+    /**
+     * This method completes the processing of editing user using PUT verb
+     *
+     * @param $request
+     * @param $channel
+     *
+     * @return $response
+     */
+    public function processEditChannel($request, $channel)
+    {
+        if (! is_null($channel)) {
+            DB::table('channels')
+            ->where('id', '=', $channel->id)
+            ->update([
+                'channel_name' => $request->input('channel_name'),
+                'channel_description' => $request->input('channel_description'),
+                'updated_at' => date('Y-m-d h:i:s'),
+            ]);
+
+            return true;
+
+        }
+        return false;
+
+    }
+
+    /**
+     * This method completes the processing of editing user using PATCH verb
+     *
+     * @param $request
+     * @param $channel
+     *
+     * @return $response
+     */
+    public function processEditChannelForPatchRequest($request, $channel)
+    {
+        if (! is_null($channel)) {
+            DB::table('channels')
+            ->where('id', '=', $channel->id)
+            ->update([
+                'channel_name' => $request->input('channel_name'),
+                'updated_at' => date('Y-m-d h:i:s'),
+        ]);
+            return true;
+            
+        }
+        return false;
+
     }
 
     /**
      * This method valdates the user request
-     * 
+     *
      * @param $request
-     * 
+     *
      * @return json $response
+     */
+    public function validateUserRequestForEmptyFields($request)
+    {
+        $validator = Validator::make($request->all(), [
+            'channel_name' => 'required',
+            'channel_description' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return true;
+
+        }
+    }
+
+    /**
+     * This method validates the user request
+     *
+     * @param $request
+     *
+     * @return boolean true
      */
     public function validateUserRequest($request)
     {
@@ -163,19 +258,40 @@ class ChannelController extends Controller
 
         if ($validator->fails()) {
             return true;
+
+        }
+    }
+
+    /**
+     * This method validates the user request
+     *
+     * @param $request
+     *
+     * @return boolean true
+     */
+    public function validateUserRequestForPatchRequest($request)
+    {
+        $validator = Validator::make($request->all(), [
+            'channel_name' => 'required|max:50',
+        ]);
+
+        if ($validator->fails()) {
+            return true;
+
         }
     }
 
     /**
      * This method validate channel request
+     * @param $pageLimit
      * @param $request
-     * @param  [type] $request [description]
-     * @return [type]          [description]
+     *
+     * @return $recordsToSkip
      */
-    public function getRecordsToSkip($perPage, $request)
+    public function getRecordsToSkip($pageLimit, $request)
     {
         $page = $request->query('page') ? : 1;
-        $recordsToSkip = (int) ($perPage * $page) - $perPage;
+        $recordsToSkip = (int) ($pageLimit * $page) - $pageLimit;
 
         return $recordsToSkip;
     }
