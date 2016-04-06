091a1acacd05297098dd5d120a2966d93b528c29
andela-tolotin <temitope.olotin@andela.com>
2016-04-06 10:01:25 +0100

    [Feature #116841693] Inject the transformer controller through setter method, Call methods from repository, re-structure message on edit user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,6 +8,7 @@ use Suyabay\Http\Requests;
 use League\Fractal\Manager;
 use Illuminate\Http\Request;
 use League\Fractal\Resource\Item;
+use Illuminate\Mail\Mailer as Mail; 
 use League\Fractal\Resource\Collection;
 use Illuminate\Support\Facades\Response;
 use Suyabay\Http\Controllers\Controller;
@@ -19,18 +20,19 @@ class UserController extends Controller
     protected $response;
     protected $fractal;
 
-    public function __construct(Manager $fractal)
+    public function __construct(Manager $fractal, Mail $mail)
     {
         $this->fractal = $fractal;
+
+        parent::__construct($mail);
     }
 
     /**
      * This method return all users to the calling API endpoint
      */
-    public function getAllUsers(Request $request)
+    public function getAllUsers(Request $request, UserTransformer $userTransformer)
     {
-        $limit = 10;
-
+        $limit = $request->query('results') ? : 10;
         $currentPage = $request->query('page') ? : 1;
 
         $recordsToSkip = (int) ($limit * $currentPage) - $limit;
@@ -47,7 +49,7 @@ class UserController extends Controller
             'avatar'
         ]);
 
-        $resource = new Collection($users, new UserTransformer);
+        $resource = new Collection($users, $userTransformer);
         $data = $this->fractal->createData($resource)->toArray();
 
         if (count($data['data']) > 0) {
@@ -55,27 +57,19 @@ class UserController extends Controller
 
         }
         
-        return Response::json(['message' => 'User record not available for display'], 404);
+        return Response::json(['message' => 'User records not available for display'], 404);
     
     }
 
     /**
      * This method return a single user to the calling API endpoint getMyDetails
      */
-    public function getSingleUser($username)
+    public function getSingleUser($username, UserTransformer $userTransformer)
     {
-        $user = User::where('username', '=', $username)
-        ->first([
-            'id',
-            'username',
-            'email',
-            'created_at',
-            'updated_at',
-            'avatar'
-        ]);
+        $user = $this->userRepository->findUserWhere($username);
 
         if (! is_null($user)) {
-            $resource = new Item($user, new UserTransformer);
+            $resource = new Item($user, $userTransformer);
             $data = $this->fractal->createData($resource)->toArray();
 
             return Response::json($data, 200);
@@ -89,22 +83,14 @@ class UserController extends Controller
     /**
      * This method return a single user to the calling API endpoint
      */
-    public function getMyDetails()
+    public function getMyDetails(UserTransformer $userTransformer)
     {
         $id  = 1; // The id will be retrieved from the token
 
-        $user = User::where('id', '=', $id)
-        ->first([
-            'id',
-            'username',
-            'email',
-            'created_at',
-            'updated_at',
-            'avatar'
-        ]);
+        $user = $this->userRepository->findUser($id)->first();
 
         if (! is_null($user)) {
-            $resource = new Item($user, new UserTransformer);
+            $resource = new Item($user, $userTransformer);
             $data = $this->fractal->createData($resource)->toArray();
 
             return Response::json($data, 200);
@@ -120,8 +106,13 @@ class UserController extends Controller
      */
     public function editUser(Request $request, $id)
     {
+        if ($this->validateUserRequestForEmptyFields($request)) {
+            return Response::json(['message' => 'All fields must be filled'], 400);
+
+        }
+
         if ($this->validateUserRequest($request)) {
-            return Response::json(['message' => 'User already exist or incomplete fields'], 400);
+            return Response::json(['message' => 'User already exists'], 400);
 
         }
 
@@ -131,7 +122,8 @@ class UserController extends Controller
         $user->save();
 
         if ($user->id) {
-            return Response::json(['message' => 'User updated successfully'], 201);
+            return Response::json(['message' => 'User updated successfully'], 200);
+
         }
 
     }
@@ -143,7 +135,6 @@ class UserController extends Controller
     {
         if (! isset($id)) {
             return Response::json(['message' => 'User id is missing'], 400);
-
         }
 
         $validator = Validator::make($request->all(), [
@@ -166,7 +157,27 @@ class UserController extends Controller
     }
 
     /**
-     * This method valdates the user request
+     * This method validates the user request for empty fields
+     * 
+     * @param $request
+     * 
+     * @return json $response
+     */
+    public function validateUserRequestForEmptyFields($request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|max:50',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return true;
+        }
+    }
+
+    /**
+     * This method validates the user request
      * 
      * @param $request
      * 
@@ -175,9 +186,8 @@ class UserController extends Controller
     public function validateUserRequest($request)
     {
         $validator = Validator::make($request->all(), [
-            'username' => 'required|unique:users|max:255',
+            'username' => 'required|unique:users',
             'email' => 'required|unique:users',
-            'password' => 'required'
         ]);
 
         if ($validator->fails()) {
