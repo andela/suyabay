8918097563bb1dcb5ac408f2d101294ba35ee28d
Raimi Ademola <ademola.raimi@andela.com>
2016-04-06 20:44:05 +0100

    [feature #116351243] fix Nickpick issues

--- app/AppInfo.php
+++ app/AppInfo.php
@@ -12,9 +12,8 @@ class AppInfo extends Model
      * @var array
      */
     protected $fillable = [
-
-    	'name',
-    	'homepage_url',
-    	'description'
+        'name',
+        'homepage_url',
+        'description'
     ];
 }
