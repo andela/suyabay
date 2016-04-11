aa8d585a06abe86b1648881e3df74add2094e0d5
andela-tolotin <temitope.olotin@andela.com>
2016-04-11 14:28:43 +0100

    [Feature #117334137] Assert the test return Json response for the current loggedin user

--- tests/UsersEndpointTest.php
+++ tests/UsersEndpointTest.php
@@ -1,11 +1,12 @@
 <?php
 
 use Suyabay\User;
+use Suyabay\Http\Repository\UserRepository;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ChannelsEndpointTest extends TestCase
+class UsersEndpointTest extends TestCase
 {
     public function testThatNothingWasReturnOnGetAllUsers()
     {
@@ -24,17 +25,37 @@ class ChannelsEndpointTest extends TestCase
 
     public function testGetASingleUser()
     {
-        $channel = factory('Suyabay\User')->create([
+        factory('Suyabay\User')->create([
             'username'       => 'ginger',
             'email'          => 'ginger@laravel.io',
             'password'       => bcrypt(str_random(10)),
             'remember_token' => str_random(10),
             'role_id'        => 1
         ]);
-        
+
         $this->get('/api/v1/users/ginger')
         ->seeJson()
         ->seeStatusCode(200);
+    }
 
+    public function testGetCurrentLoggedInUserInfo()
+    {
+        $user = factory('Suyabay\User')->create([
+            'username'       => 'laztopaz',
+            'email'          => 'temitope.olotin@laravel.io',
+            'password'       => bcrypt(str_random(10)),
+            'remember_token' => str_random(10),
+            'role_id'        => 1
+        ]);
+
+        $user = UserRepository::findUser($user['id'])->toArray();
+        $this->assertTrue(is_array($user));
+        $this->assertArrayHasKey('id', $user);
+        $this->assertArrayHasKey('role_id', $user);
+        $this->assertArrayHasKey('username', $user);
+
+        $this->get('/api/v1/users/me')
+        ->seeJson()
+        ->seeStatusCode(200);
     }
 }
