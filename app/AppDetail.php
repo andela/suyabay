27073dbd96cfb0db70fe2769edb0502d102e54ec
Raimi Ademola <ademola.raimi@andela.com>
2016-04-13 07:08:36 +0100

    [feature #116873071] edit appDetail's model comment

--- app/AppDetail.php
+++ app/AppDetail.php
@@ -22,7 +22,7 @@ class AppDetail extends Model
     /**
      * Define the relationship between user to AppDetails table.
      *
-     * return object
+     * @return object
      */
     public function user()
     {
