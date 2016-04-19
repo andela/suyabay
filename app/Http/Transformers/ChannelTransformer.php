c8f6db77d7deed6daf1dbc80ea7b1d4419c38c06
andela-tolotin <temitope.olotin@andela.com>
2016-04-19 22:08:57 +0100

    [Feature #116864429] Rename user_id to channel_id

--- app/Http/Transformers/ChannelTransformer.php
+++ app/Http/Transformers/ChannelTransformer.php
@@ -9,7 +9,7 @@ class ChannelTransformer extends Fractal\TransformerAbstract
     public function transform(Channel $channel)
     {
         return [
-        'user_id'            => (int) $channel->id,
+        'channel_id'            => (int) $channel->id,
         'channel_name'       => $channel->channel_name,
         'channel_note'       => $channel->channel_description,
         'subscription_count' => $channel->subscription_count,
