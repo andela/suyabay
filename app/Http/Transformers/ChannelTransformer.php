80c3a7865f5319527d159d8fab5337d5c5a2e79c
andela-tolotin <temitope.olotin@andela.com>
2016-04-25 11:11:21 +0100

    [Feature #116864429] Remove user id

--- app/Http/Transformers/ChannelTransformer.php
+++ app/Http/Transformers/ChannelTransformer.php
@@ -9,15 +9,14 @@ class ChannelTransformer extends Fractal\TransformerAbstract
 {
     public function transform(Channel $channel)
     {
-        $user = UserRepository::findUser($channel->user_id)->first();
-
         return [
         'channel_id'         => (int) $channel->id,
         'channel_name'       => $channel->channel_name,
         'channel_note'       => $channel->channel_description,
         'date_created'       => $channel->created_at,
         'date_modified'      => $channel->updated_at,
-        'created_by'         => $user->username
+        'created_by'         => $channel->username,
+        'episodes'           => $channel->episodes,
         ];
     }
 }
