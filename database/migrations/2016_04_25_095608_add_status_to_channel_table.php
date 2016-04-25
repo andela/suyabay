f8f302b7e95c605c8f65f8c7fbf5a745838ccd9c
andela-tolotin <temitope.olotin@andela.com>
2016-04-25 11:03:02 +0100

    [Feature #116864429] Create a migration that added a status field to channels table

---
+++ database/migrations/2016_04_25_095608_add_status_to_channel_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStatusToChannelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->integer('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
