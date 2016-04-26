af3b8add36e5643d98c79e4a30e7011522ba63d4
Raimi Ademola <ademola.raimi@andela.com>
2016-04-26 18:10:51 +0100

    [feature #118376691] modify api.js to fix undefined clipboard

--- public/js/api.js
+++ public/js/api.js
@@ -1,5 +1,5 @@
-
-	var  clipboard = new Clipboard(".copy");
+	var copy = document.getElementById('copy');
+	var  clipboard = new Clipboard(copy);
 
 	clipboard.on("success", function() {
     	swal("Token copied successfully");
