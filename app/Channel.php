ac63b52321e52e6b2282a636ecc0b3a597f8fa1e
andela-tolotin <temitope.olotin@andela.com>
2016-04-26 20:37:26 +0100

    [Feature #116864429] Create query scope for pending and active episodes

--- app/Channel.php
+++ app/Channel.php
@@ -38,4 +38,19 @@ class Channel extends Model
     {
         return $this->hasMany('Suyabay\Episode');
     }
+
+    public function scopePendingEpisodes($query, $id)
+    {
+        return $query->where('channels.id', '=', $id)
+                    ->join('episodes', 'channels.id', '=', 'episodes.channel_id')
+                    ->where('episodes.status', '=', '0');
+    }
+
+    public function scopeActiveEpisodes($query, $id)
+    {
+        return $query->where('channels.id', '=', $id)
+                    ->join('episodes', 'channels.id', '=', 'episodes.channel_id')
+                    ->where('episodes.status', '=', '1');
+    }
+    
 }
