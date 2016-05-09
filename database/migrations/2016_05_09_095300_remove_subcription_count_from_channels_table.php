07e825acbe057583b8d8744500b126eeb406bdd2
andela-tolotin <temitope.olotin@andela.com>
2016-05-09 10:59:17 +0100

    [bg #119172335] Create a migration that removes subscription count from channels table

---
+++ database/migrations/2016_05_09_095300_remove_subcription_count_from_channels_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveSubcriptionCountFromChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->dropColumn('subscription_count');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            
+        });
+    }
+}
