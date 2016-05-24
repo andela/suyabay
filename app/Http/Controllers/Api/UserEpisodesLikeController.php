ff5428be872305792a83a1633502f30046e2c6ae
andela-tolotin <temitope.olotin@andela.com>
2016-05-24 13:06:27 +0100

    [Feature #119981207] Create a method that will return the user liked episodes

---
+++ app/Http/Controllers/Api/UserEpisodesLikeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Suyabay\Http\Controllers\Api;
+
+use Validator;
+use Suyabay\User;
+use Suyabay\Favourite;
+use Suyabay\Http\Requests;
+use League\Fractal\Manager;
+use Illuminate\Http\Request;
+use League\Fractal\Resource\Item;
+use Illuminate\Mail\Mailer as Mail;
+use Illuminate\Support\Facades\Response;
+use Suyabay\Http\Controllers\Controller;
+use Suyabay\Http\Repository\UserRepository;
+use Suyabay\Http\Transformers\UserTransformer;
+
+class UserEpisodesLikeController extends Controller
+{
+    protected $mail;
+    protected $response;
+    protected $fractal;
+
+    public function __construct(Manager $fractal, Mail $mail)
+    {
+        $this->fractal = $fractal;
+        parent::__construct($mail);
+    }
+
+    /**
+     * This method get all the episode podcast liked by a user.
+     *
+     * @param $username
+     *
+     * @return like
+     */
+    public function getUserLikedEpisodes($username)
+    {
+        $user = User::where('username', urldecode($username))
+        ->get()
+        ->first();
+        if (is_null($user)) {
+            return Response::json(['message' => 'User not found!'], 404);
+
+        }
+
+        $likes = $user->likes()->get();
+
+        if (count($likes) > 0) {
+             return Response::json($this->formatUserEpisodeLikes($likes), 200);
+        }
+
+        return Response::json(['message' => 'User has 0 episode like(s)!'], 404);
+    }
+}
