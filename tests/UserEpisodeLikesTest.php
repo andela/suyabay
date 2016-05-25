57c819be6e2c39e24fb90789b1974a7b7f12f90d
andela-tolotin <temitope.olotin@andela.com>
2016-05-25 11:40:19 +0100

    [Feature #119981207] Write a test that asserts that username does not exist

--- tests/UserEpisodeLikesTest.php
+++ tests/UserEpisodeLikesTest.php
@@ -24,5 +24,26 @@ class UserEpisodeLikesTest extends TestCase
         $this->assertEquals($decodedResponse->message, 'User not found!');
         $this->assertEquals($response->status(), 404);
     }
-    
+
+    public function testThatUserHasLikedEpisodes()
+    {
+        $user = factory('Suyabay\User')->create();
+        $episode = factory('Suyabay\Episode')->create();
+        $likes = factory('Suyabay\Like')->create([
+            'user_id'    => $user->id ,
+            'episode_id' => $episode->id,
+        ]);
+
+        $response = 
+        $this->actingAs($user)
+        ->call('GET', '/api/v1/users/'.$user->username.'/favourites');
+
+        $decodedResponse = json_decode($response->getContent());
+
+        var_dump(expression)
+        exit();
+
+        $this->assertEquals($decodedResponse->message, 'User not found!');
+        $this->assertEquals($response->status(), 200);
+    }
 }
