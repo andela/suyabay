64bc578d3ef928b92ad6134edc2faa3252820f38
andela-tolotin <temitope.olotin@andela.com>
2016-05-25 11:40:05 +0100

    [Feature #119981207] Write a test that asserts that username does not exist

--- tests/UserEpisodeLikesTest.php
+++ tests/UserEpisodeLikesTest.php
@@ -6,5 +6,23 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UserEpisodeLikesTest extends TestCase 
 {
+    public function testThatUsernameSuppliedIsCorrect()
+    {
+        $user = factory('Suyabay\User')->create();
+        $episode = factory('Suyabay\Episode')->create();
+        $likes = factory('Suyabay\Like')->create([
+            'user_id'    => $user->id ,
+            'episode_id' => $episode->id,
+        ]);
+
+        $response = 
+        $this->actingAs($user)
+        ->call('GET', '/api/v1/users/Laztopaz/favourites');
+
+        $decodedResponse = json_decode($response->getContent());
+
+        $this->assertEquals($decodedResponse->message, 'User not found!');
+        $this->assertEquals($response->status(), 404);
+    }
     
 }
