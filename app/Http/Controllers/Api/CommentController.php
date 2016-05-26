1b7233ad9f406bcf1dc24f3c56354ddc5afec5f5
Raimi Ademola <ademola.raimi@andela.com>
2016-05-26 10:49:27 +0100

    [feature #120180435] remove white space detected by nitpick

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -38,7 +38,7 @@ class CommentController extends Controller
      * @param $name
      * @param $request
      * @param $commentTransformer
-     * 
+     *
      * @return json $response
      */
     public function getEpisodeComments($name, Request $request, EpisodeTransformer $episodeTransformer, CommentTransformer $commentTransformer)
