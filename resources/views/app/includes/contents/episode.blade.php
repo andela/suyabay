51930df969737901486424c787e7bf9b04eff86c
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-06-16 11:55:33 +0100

    [Fix #121655437] Extend player to all episodes
    The audio is extended to all episodes.
    A new blade partial is included so that all changes to the view of
    an episode are made once.

--- resources/views/app/includes/contents/episode.blade.php
+++ resources/views/app/includes/contents/episode.blade.php
@@ -10,24 +10,7 @@
 
         @if ($episodes->count() > 0)
             @foreach($episodes as $podcast)
-            <a style="color:#2C3E50" href="/episodes/{{$podcast->id}}">
-                <div class="row podcast">
-                    <div class="col s3">
-                        <img class="responsive-img podcast-img" src="{{ asset($podcast->image) }}">
-                    </div>
-
-                    <div class="col s9 details">
-                        <span class="podcast-episode-date">{{ $podcast->created_at->diffForHumans() }}</span>
-                        <span class="tag podcast-episode-date">{{$podcast->channel->channel_name}}</span>
-                        <h5 class="podcast-episode-title">{{ $podcast->episode_name }}</h5>
-
-                        <p>
-                            {{$podcast->episode_description}}
-                        </p>
-
-                    </div>
-                </div>
-            </a>
+                @include('app.includes.contents.podcast')
             @endforeach
         @else
             <div class="col s12">
--- resources/views/app/includes/contents/main.blade.php
+++ resources/views/app/includes/contents/main.blade.php
@@ -10,77 +10,8 @@
 
         <h4 class="center-align padcast-page-header" style="margin-bottom:50px;">Podcast for Suya lovers</h4>
 
-        <div class="row podcast">
-            <div class="col s3">
-                <a style="color:#2C3E50" href="/episodes/{{$episodes->first()->id}}">
-                    <img class="responsive-img podcast-img" src="{!! asset($episodes->first()->image) !!}">
-                </a>
-            </div>
-
-            <div class="col s9 details">
-                <span class="podcast-episode-date">{{$episodes->first()->created_at->diffForHumans()}}</span>
-                <span class="tag podcast-episode-date">{{$episodes->first()->channel->channel_name}}</span>
-                <a style="color:#2C3E50" href="/episodes/{{$episodes->first()->id}}">
-                    <h5 class="podcast-episode-title">{{$episodes->first()->episode_name}}</h5>
-                </a>
-                <div>
-                    <audio width="10px;" src="{{$episodes->first()->audio_mp3}}" preload="auto" />
-                </div>
-                <p>
-                     {{$episodes->first()->episode_description}}
-                </p>
-
-                <div class="podcast-actions right">
-
-                    <input type="hidden" id="token" name="_token" value="{{ csrf_token() }}">
-
-                    @if( Auth::check() )
-                        <input type="hidden" id="user_id" value="{{ Auth::user()->id }}" >
-                    @endif
-
-                    <input type="hidden" id="episode_id" value="{{ $episodes->first()->id }}">
-
-                    <span style="padding-right:15px;">
-                         <i class="fa fa-heart social-btn like-btn {{ $episodes->first()->like_status }}" like-status="{{ $episodes->first()->like_status }}" data-episode-id="{{ $episodes->first()->id }}"> {{ $episodes->first()->likes }}</i>
-                    </span>
-
-                    <span style="padding-right:15px;">
-                        <i class="fa fa-comment social-btn like"> {{ $episodes->first()->comment->count() }}</i>
-                    </span>
-
-                    <span style="padding-right:15px;">
-                        <a href="#" class="twtr-share" data-desc="{{ $episodes->first()->episode_description }}" data-name="{{ $episodes->first()->episode_name }}" data-img="{!! asset($episodes->first()->image) !!}" data-url="{!! url('/episodes', $episodes->first()->id)  !!}">
-                            <i class="fa fa-twitter social-btn "></i>
-                        </a>
-                    </span>
-                    <span style="padding-right:15px;">
-                        <a href="#" class="fb-share" data-desc="{{ $episodes->first()->episode_description }}" data-name="{{ $episodes->first()->episode_name }}" data-img="{!! asset($episodes->first()->image) !!}" data-url="{!! url('/episodes', $episodes->first()->id) !!}">
-                            <i class="fa fa-facebook social-btn "></i>
-                        </a>
-                    </span>
-                </div>
-            </div>
-        </div>
-
         @foreach($episodes->take(-4) as $podcast)
-        <a style="color:#2C3E50" href="/episodes/{{$podcast->id}}">
-            <div class="row podcast">
-                <div class="col s3">
-                    <img class="responsive-img podcast-img" src="{{ asset($podcast->image) }}">
-                </div>
-
-                <div class="col s9 details">
-                    <span class="podcast-episode-date">{{ $podcast->created_at->diffForHumans() }}</span>
-                    <span class="tag podcast-episode-date">{{$podcast->channel->channel_name}}</span>
-                    <h5 class="podcast-episode-title">{{ $podcast->episode_name }}</h5>
-
-                    <p>
-                        {{$podcast->episode_description}}
-                    </p>
-
-                </div>
-            </div>
-        </a>
+            @include('app.includes.contents.podcast')
         @endforeach
 
         <p class="center-align">
