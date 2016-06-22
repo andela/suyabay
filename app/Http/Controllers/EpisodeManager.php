94bef058ee76225c0aeade33bc63ab29dcdd2bb8
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-06-22 15:07:05 +0100

    Comment out the podcast validation

--- app/Http/Controllers/EpisodeManager.php
+++ app/Http/Controllers/EpisodeManager.php
@@ -156,7 +156,7 @@ class EpisodeManager extends Controller
             'title'         => 'required|min:3',
             'description'   => 'required',
             'channel'       => 'required',
-            'podcast'       => 'required|size_format|mimes:mpga'
+            // 'podcast'       => 'required|size_format|mimes:mpga'
         ]);
 
         $data    = [
