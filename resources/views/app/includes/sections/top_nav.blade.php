f21d5165f379a402e146eefd1c78cd1ed0da6329
andela-tolotin <temitope.olotin@andela.com>
2016-06-28 12:45:45 +0100

    [Feature #122113191] Create a blade file where all user requests will be display

--- resources/views/app/includes/sections/top_nav.blade.php
+++ resources/views/app/includes/sections/top_nav.blade.php
@@ -21,7 +21,6 @@
                         <p class="username"> {{ ucwords(Auth::user()->username) }} </p>
                         <i class="material-icons right">arrow_drop_down</i>
                         </a>
-
                         @if( Auth::user()->hasChannelNotifications())
                             <a class="waves-effect waves-light modal-trigger" href="{{ route('notifications') }}"> <span class="badge" id="notification-num">{{ Auth::user()->newChannelsCount() }} </span> New {{ str_plural('Channel', Auth::user()->newChannelsCount()) }}</a>
                         @endif
