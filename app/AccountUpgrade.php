7bd96e076753e5daf5f79cfbac05ae525e2bc725
andela-tolotin <temitope.olotin@andela.com>
2016-06-28 11:05:23 +0100

    [Feature #122113191] Add reason field to fillable

--- app/AccountUpgrade.php
+++ app/AccountUpgrade.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AccountUpgrade extends Model
 {
-    protected $fillable = ['user_id', 'request'];
+    protected $fillable = ['user_id', 'reason'];
 
     public function user()
     {
