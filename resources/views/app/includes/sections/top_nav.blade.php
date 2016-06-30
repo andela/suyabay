529882198e5143603f541c7520dac71e78545901
andela-tolotin <temitope.olotin@andela.com>
2016-06-30 11:28:23 +0100

    [Feature #122113191] Fix dead link on premium user achor tag

--- resources/views/app/includes/sections/top_nav.blade.php
+++ resources/views/app/includes/sections/top_nav.blade.php
@@ -36,12 +36,11 @@
                         @endcan
 
                         @can( 'see-upgrade', Auth::user()->role->name )
-                            <a class="waves-effect teal lighten-2" href="#">Become a Premium User</a>
+                            <a class="waves-effect teal lighten-2" href=" {{ URL::to('request-premium') }}">Become a Premium User</a>
                         @endcan
                     @else
                     <a class="waves-effect modal-trigger" href="{{ URL::to('login') }}">Log In</a>
                     <a class="waves-effect waves-light modal-trigger" href="{{ URL::to('signup') }}">Sign Up</a>
-                    <a class="waves-effect teal lighten-2" href="{{ URL::to('signup') }}">Become a Premium User</a>
                     @endif
                 </li>
             </ul>
